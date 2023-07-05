fa54306b56aca5ef0a421de22e12066193b6454a
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2023-07-05 22:05:12 +0300

    Time: 0 ms (100%), Space: 39.3 MB (74.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return count(n, memo);
-    }
-
-    public int count(int n, int[] memo) {
-        if (n == 0) {
-            return 1;
+        int a = 1;
+        int b = 1;
+        for (int i = 2; i < n + 1; ++i) {
+            int temp = a;
+            a = b;
+            b = temp + b;
         }
 
-        if (n == 1) {
-            return 1;
-        }
-
-        if (memo[n] > 0) {
-            return memo[n];
-        }
-
-        memo[n] = count(n - 1, memo) + count(n - 2, memo);
-        return memo[n];
+        return b;
     }
 }
