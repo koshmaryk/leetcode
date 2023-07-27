8c6a05852efc4ed982025335d52c01c3b698bbdf
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2023-07-27 21:28:18 +0300

    Time: 0 ms (100.00%), Space: 39.3 MB (66.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int a = 1;
-        int b = 1;
+        int s1 = 1;
+        int s2 = 1;
         for (int i = 2; i < n + 1; ++i) {
-            int temp = a;
-            a = b;
-            b = temp + b;
+            int temp = s2;
+            s2 = s2 + s1;
+            s1 = temp;
         }
-
-        return b;
+        
+        return s2;
     }
 }
