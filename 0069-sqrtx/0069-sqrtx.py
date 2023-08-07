12075dda99eb7ac5a5583ddba856b48eaf201dc8
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2023-08-07 21:57:08 +0300

    Time: 40 ms (93.79%), Space: 16.1 MB (90.72%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        bad = 0
+        good = x
+        while good - bad > 0.00001:
+            mid = (bad + good) / 2
+            if mid * mid >= x:
+                good = mid
+            else:
+                bad = mid
+        
+        return int(good)
