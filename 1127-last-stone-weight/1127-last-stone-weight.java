916ce6314595278b19f05f498bba00f91f496407
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2023-09-11 23:26:06 +0300

    Time: 1 ms (99%), Space: 40.1 MB (31.94%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            heap.offer(stone);
+        }
+
+        while (heap.size() > 1) {
+            int y = heap.poll();
+            int x = heap.poll();
+
+            if (x != y) {
+                heap.offer(y - x);
+            }
+        }
+        
+
+        return heap.isEmpty() ? 0 : heap.poll();
+    }
+}
