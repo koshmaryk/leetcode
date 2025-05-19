7775e9975f23f25263a960eb7730dfa60e8b7c2e
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-19 22:33:51 +0300

    Time: 63 ms (56.72%), Space: 18.1 MB (42.45%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,16 +1,33 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def validPalindromeHelper(l: int, r: int, i: int) -> bool:
-            while l < r:
-                if s[l] != s[r]:
-                    if i > 0:
-                        return validPalindromeHelper(l + 1, r, 0) or validPalindromeHelper(l, r - 1, 0)
-                    else:
-                        return False
-                l += 1
-                r -= 1
-                
-            return True
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                right = True
+                l1, r1 = l + 1, r
+                while l1 < r1:
+                    if s[l1] != s[r1]:
+                        right = False
+                        break
+                    l1 += 1
+                    r1 -= 1
 
-        return validPalindromeHelper(0, len(s) - 1, 1)
+                left = True
+                l2, r2 = l, r - 1
+                while l2 < r2:
+                    if s[l2] != s[r2]:
+                        left = False
+                        break
+                    l2 += 1
+                    r2 -= 1
+
+                if left or right:
+                    return True
+                else:
+                    return False
+
+            l += 1
+            r -= 1
+                
+        return True
         
