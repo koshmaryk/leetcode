ac2fe6d34260048b362be2b2cd7886b1ab706682
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-19 22:35:02 +0300

    Time: 67 ms (34.77%), Space: 18 MB (62.14%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -3,25 +3,25 @@ class Solution:
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                right = True
+                skip1 = True
                 l1, r1 = l + 1, r
                 while l1 < r1:
                     if s[l1] != s[r1]:
-                        right = False
+                        skip1 = False
                         break
                     l1 += 1
                     r1 -= 1
 
-                left = True
+                skip2 = True
                 l2, r2 = l, r - 1
                 while l2 < r2:
                     if s[l2] != s[r2]:
-                        left = False
+                        skip2 = False
                         break
                     l2 += 1
                     r2 -= 1
 
-                if left or right:
+                if skip1 or skip2:
                     return True
                 else:
                     return False
