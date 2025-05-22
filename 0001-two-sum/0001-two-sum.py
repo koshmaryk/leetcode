383406e8fe114ae739c07557b447b2cb28282b19
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-22 21:54:23 +0300

    Time: 3 ms (50.75%), Space: 19 MB (24.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,9 +3,8 @@ class Solution:
         n = len(nums)
         kv = {}
         for i in range(n):
-            kv[nums[i]] = i
-        for i in range(n):
             diff = target - nums[i]
-            if diff in kv and kv[diff] != i:
-                return [i, kv[diff]]
+            if diff in kv:
+                return [kv[diff], i]
+            kv[nums[i]] = i
         return []
