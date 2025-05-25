2eade73a99e0a596ab288fd69546c02e9c7c10ad
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-25 14:56:40 +0300

    Time: 7 ms (47.51%), Space: 19.2 MB (50.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -4,26 +4,28 @@ from heapq import heappush, heappop
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         G = defaultdict(list)
+        # O(V)
         in_degrees = [0] * numCourses
+        # O(E)
         for course, prerequisite in prerequisites:
             G[prerequisite].append(course)
             in_degrees[course] += 1
 
-        Q = []
+        Q = [] # min priority queue
+        # O(V log V)
         for u in range(numCourses):
             if in_degrees[u] == 0:
-                heappush(Q, u)
+                heappush(Q, u) # smaller id goes first
 
-        #ts = []
         visited = 0
+        # O(V log V + E)
         while Q:
             u = heappop(Q)
-            #ts.append(u)
             visited += 1
             for v in G[u]:
                 in_degrees[v] -= 1 # virtually remove u -> v
-                if in_degrees[v] == 0:
-                    heappush(Q, v)
+                if in_degrees[v] == 0: # v is next candidate
+                    heappush(Q, v) # smaller id goes first
 
         return visited == numCourses
         
