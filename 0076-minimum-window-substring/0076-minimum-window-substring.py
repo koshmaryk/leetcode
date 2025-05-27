e6c816d2fcfa8c1d512de8742a1c5635cd8b2111
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-27 16:14:44 +0300

    Time: 281 ms (16.86%), Space: 18.4 MB (16.46%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        #                   |
+        # A D O B E C O D E B A N C
+        # |
+        # A B C
+        #
+        m, n = len(s), len(t)
+        ans = ""
+        if m < n:
+            return ans
+
+        count = {}
+        for i in range(n):
+            count[t[i]] = count.get(t[i], 0) + 1
+        
+        l = 0
+        for r in range(m):
+            if s[r] in count:
+                count[s[r]] -= 1
+
+            while l <= r and max(count.values()) <= 0:
+                if not ans or r - l + 1 < len(ans):
+                    ans = s[l:r+1]
+                    
+                if s[l] in count:
+                    count[s[l]] += 1
+                l += 1
+
+        return ans
