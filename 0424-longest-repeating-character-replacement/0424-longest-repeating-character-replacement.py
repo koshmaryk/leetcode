facf94cb7a6a12bff4bac9fa9e3ea3f796e011f0
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-27 10:14:34 +0300

    Time: 175 ms (33.39%), Space: 18.1 MB (23.45%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,15 +4,13 @@ class Solution:
         # A B A A
         # longest = 0
         # 3 < 4?
-        longest = 0
+        ans = 0
         count = {}
         l = 0
         for r in range(len(s)):
             count[s[r]] = count.get(s[r], 0) + 1
-            current_max = max(count.values()) # O(26)
-            while r - l + 1 - current_max > k:
+            while r - l + 1 - max(count.values()) > k:
                 count[s[l]] = count.get(s[l], 0) - 1
                 l += 1
-            if r - l + 1 - current_max <= k:
-                longest = max(longest, r - l + 1)
-        return longest
+            ans = max(ans, r - l + 1)
+        return ans
