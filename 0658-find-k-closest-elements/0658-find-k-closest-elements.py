349bd215e35a44da49b9a531319cd8a5ad47f33e
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-05-30 00:05:27 +0300

    Time: 1 ms (72.64%), Space: 19 MB (83.42%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,13 +1,23 @@
 class Solution:
+    # given a sorted input array, it means we have to answer "what's the optimal window position?"
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         if k >= len(arr): return arr
         if x <= arr[0]: return arr[:k]
         if x >= arr[-1]: return arr[-k:]
+
         bad, good = -1, len(arr) - k
         while good - bad > 1:
             mid = (bad + good) // 2
-            if x - arr[mid] <= arr[mid + k] - x:
+            # x ...arr[mid] ... | ... arr[mid + k]... x
+            if x <= (arr[mid] + arr[mid + k]) // 2:
                 good = mid
             else:
                 bad = mid
         return arr[good:good + k]
+
+# k = 4, x = 3
+#                 
+# -1 ... 1,2,3,4,5, ... 6,7,8,9 ... 10
+#
+# bad = -1, good = 0, mid = 2
+# 3<=5?; 3<=3? 
