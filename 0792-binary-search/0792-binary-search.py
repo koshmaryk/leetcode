553a1b3b8c24ee28c534c1c4a60f3afcd7c05869
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-06 01:04:02 +0300

    Time: 0 ms (100%), Space: 18.7 MB (73.85%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,16 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
+        # 
+        # -1,0,3,5,9,12
+        #  0 1 2 3 4 5
+        # bad = 1
+        # good = 2
         bad = -1
-        good = n
+        good = len(nums)
         while good - bad > 1:
             mid = (bad + good) // 2
             if nums[mid] >= target:
                 good = mid
             else:
                 bad = mid
-
-        if good == n or nums[good] != target:
-            return -1
-        else:
-            return good
+        return good if good < len(nums) and nums[good] == target else -1
+        
