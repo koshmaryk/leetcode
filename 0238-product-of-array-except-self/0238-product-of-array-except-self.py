118d5a6d562e27f93796b5acb0af04a69c32642c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-08 22:38:25 +0300

    Time: 35 ms (26.22%), Space: 27.1 MB (11.72%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,14 +7,16 @@ class Solution:
         n = len(nums)
         answer = [0] * n
 
-        p = s = 1
-        for i in range(n):
-            answer[i] = p
-            p *= nums[i]
+        p = [1] * (n + 1)
+        for i in range(1, n):
+            p[i] = p[i - 1] * nums[i - 1]
 
-        for i in range(n - 1, - 1, -1):
-            answer[i] *= s 
-            s *= nums[i]
+        s = [1] * (n + 1)
+        for i in range(n - 2, -1, -1):
+            s[i] = s[i + 1] * nums[i + 1]
+
+        for i in range(n):
+            answer[i] = p[i] * s[i]
         
         # (product to the left of i) * (product to the right of i)
         return answer
