41502ce27b8de091a3cf72ace21eac50b6952d91
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-09 20:39:29 +0300

    Time: 9 ms (29.29%), Space: 20.5 MB (39.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,13 +1,34 @@
-from collections import defaultdict
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        freq = defaultdict(int)
+        # 3 * (n/3 + 1) = n + 3
+        n = len(nums)
+        candidate1 = candidate2 = float('inf')
+        count1 = count2 = 0
         for num in nums:
-            freq[num] += 1
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
 
+        count1 = count2 = 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+        
         output = []
-        for num, count in freq.items():
-            if count > len(nums) // 3:
-                output.append(num)
+        if count1 > n // 3:
+            output.append(candidate1)
+        if count2 > n // 3:
+            output.append(candidate2)
         return output
