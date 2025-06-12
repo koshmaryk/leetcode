4469397829065590a62d53bb0f5f67ab40d617d9
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-12 14:57:25 +0300

    Time: 0 ms (100%), Space: 23.5 MB (12.37%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,26 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from collections import deque
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next:
-            return
+        # 1,2,3,4,5
+        slow = fast = head # 1; 1 | 2; 3 | 3;5
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        q = deque()
-        curr = head
+        curr = slow.next # 4
+        prev = slow.next = None # don't forget to remove link between l1 and l2
         while curr:
-            q.append(curr)
-            curr = curr.next
-        
-        curr = q.popleft()
-        i = 1
-        while q:
-            curr.next = q.popleft() if i % 2 == 0 else q.pop()
-            curr = curr.next
-            i += 1
+            tmp = curr.next # 5 | None
+            curr.next = prev # None <- 4 | None <- 4 <- 5
+            prev = curr # 4 | 5
+            curr = tmp # 5 | None
 
-        curr.next = None
+        # l1 = 1,2,3
+        # l2 = 5, 4
+        l1, l2 = head, prev
+        while l2:
+            tmp1, tmp2 = l1.next, l2.next # 2,3; 4 | 3, None
+            l1.next = l2 # 1 -> 5,4 | .. 2 -> 4
+            l2.next = tmp1 # 5 -> 2,3 | .. 4 -> 3
+            l1, l2 = tmp1, tmp2 # 2,3; 4 | 3, None
+
+        
