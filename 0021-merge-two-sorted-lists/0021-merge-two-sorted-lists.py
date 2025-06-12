1e5349b5000a6350e4a361e9bcb5aae7ea363356
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-12 09:58:44 +0300

    Time: 0 ms (100%), Space: 17.6 MB (88.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -14,12 +14,10 @@ class Solution:
             else:
                 prev.next = list2
                 list2 = list2.next
+            
+            prev = prev.next
 
-            prev= prev.next
-        
-        if list1:
-            prev.next = list1
-        if list2:
-            prev.next = list2
+        if list1 or list2:
+            prev.next = list1 if list1 else list2
 
         return sentinel.next
