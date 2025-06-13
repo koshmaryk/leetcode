f971fc1853a06ac5b10be54379659a9d8ca14fd8
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-14 00:02:45 +0300

    Time: 9 ms (14.55%), Space: 17.9 MB (56.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,19 +8,17 @@ class Solution:
         sentinel = ListNode()
         curr = sentinel
         carry = 0
-        while l1 or l2:
+        while l1 or l2 or carry:
             num1 = l1.val if l1 else 0
             num2 = l2.val if l2 else 0
             num = num1 + num2 + carry
-            carry = num // 10
 
             curr.next = ListNode(num % 10)
             curr = curr.next
 
+            carry = num // 10
+
             l1 = l1.next if l1 else l1
             l2 = l2.next if l2 else l2
-
-        if carry == 1:
-            curr.next = ListNode(carry)
 
         return sentinel.next
