69c6c09125ba5722ebe963a4b6cbf4364e4b79fe
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-26 01:35:38 +0300

    Time: 82 ms (76.1%), Space: 21.5 MB (41.93%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -4,50 +4,51 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 
 class Codec:
 
     def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
+        if not root:
+            return "nil"
+
         preorder = []
-        def dfs(node):
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
             if not node:
                 preorder.append("nil")
-                return
+                continue
 
             preorder.append(str(node.val))
-            dfs(node.left)
-            dfs(node.right)
+            queue.append(node.left)
+            queue.append(node.right)
 
-        dfs(root)
         return ",".join(preorder)
         
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-        preorder = data.split(",")
-        index = -1
-        def dfs():
-            nonlocal index
-            
-            index += 1
-            if preorder[index] == "nil":
-                return None
-            
-            node = TreeNode(int(preorder[index]))
-            node.left = dfs()
-            node.right = dfs()
-            return node
+        if data == "nil":
+            return None
 
-        return dfs()
+        preorder = data.split(",")
+        root = TreeNode(int(preorder[0]))
+        queue = deque([root])
+        index = 1
+        while queue:
+            curr = queue.popleft()
+
+            if preorder[index] != "nil":
+                curr.left = TreeNode(int(preorder[index]))
+                queue.append(curr.left)
+            index += 1
+
+            if preorder[index] != "nil":
+                curr.right = TreeNode(int(preorder[index]))
+                queue.append(curr.right)
+            index += 1
+
+        return root
 
         
 
