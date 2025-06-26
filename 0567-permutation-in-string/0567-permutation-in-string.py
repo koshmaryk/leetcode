dbb1205f157e62f2f1c9a2698e2ba244d6a359c6
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-26 21:02:12 +0300

    Time: 16 ms (73.79%), Space: 17.8 MB (87.39%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,11 +1,13 @@
+# s1Count = 1 1 0 0 0 ... 0 ...
+# s2Count = 1 1 0 0 0 ... 0 ...
+# matches == 26
+# ab
+# 
+# e i d b a o o o
 class Solution:
-    # ab
-    #
-    #   |   |
-    # e i b d b a o o o
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n1, n2 = len(s1), len(s2)
-        if n2 < n1:
+        if n1 > n2:
             return False
 
         s1Count, s2Count = [0] * 26, [0] * 26
@@ -19,7 +21,8 @@ class Solution:
 
         l = 0
         for r in range(n1, n2):
-            if matches == 26: return True
+            if matches == 26:
+                return True
 
             i = ord(s2[r]) - ord('a')
             s2Count[i] += 1
@@ -35,6 +38,5 @@ class Solution:
             elif s1Count[i] - 1 == s2Count[i]:
                 matches -= 1
             l += 1
-        
+
         return matches == 26
-        
