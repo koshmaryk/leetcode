91d682e791ad622ea41722c7d4bdfa949cb516fd
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-27 13:14:04 +0300

    Time: 27 ms (48.1%), Space: 18 MB (18.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,42 +1,36 @@
-# s1Count = 1 1 0 0 0 ... 0 ...
-# s2Count = 1 1 0 0 0 ... 0 ...
-# matches == 26
+from collections import defaultdict
+
 # ab
-# 
+# need: a=1, b=1
+# window: b=1, a=1
+# matches = 0
+#
+# l = 3, r = 4
 # e i d b a o o o
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1, n2 = len(s1), len(s2)
-        if n1 > n2:
-            return False
-
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(n1):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
+        need = defaultdict(int)
+        for c in s1:
+            need[c] += 1
 
         matches = 0
-        for i in range(26):
-            matches += 1 if s1Count[i] == s2Count[i] else 0
+        window = defaultdict(int)
 
         l = 0
-        for r in range(n1, n2):
-            if matches == 26:
+        for r in range(len(s2)):
+            if s2[r] in need:
+                window[s2[r]] += 1
+                if window[s2[r]] == need[s2[r]]:
+                    matches += 1
+            
+            while r - l + 1 > len(s1):
+                if s2[l] in need:
+                    if window[s2[l]] == need[s2[l]]:
+                        matches -= 1
+                    window[s2[l]] -= 1
+
+                l += 1
+
+            if r - l + 1 == len(s1) and matches == len(need):
                 return True
-
-            i = ord(s2[r]) - ord('a')
-            s2Count[i] += 1
-            if s1Count[i] == s2Count[i]:
-                matches += 1
-            elif s1Count[i] + 1 == s2Count[i]:
-                matches -= 1
-
-            i = ord(s2[l]) - ord('a')
-            s2Count[i] -= 1
-            if s1Count[i] == s2Count[i]:
-                matches += 1
-            elif s1Count[i] - 1 == s2Count[i]:
-                matches -= 1
-            l += 1
-
-        return matches == 26
+        return False
