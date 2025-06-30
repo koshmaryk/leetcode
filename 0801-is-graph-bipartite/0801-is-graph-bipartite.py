718a57eff42098a24bc69caa83b23bd7fe66fa42
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-30 18:01:16 +0300

    Time: 3 ms (43.75%), Space: 18.9 MB (9.19%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,19 +1,27 @@
+from collections import deque
+
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         colors = {}
 
-        def bipartite(u, color):
-            if u in colors:
-                return colors[u] == color
+        def bipartite(node):
+            queue = deque([(node, 1)])
+            while queue:
+                u, color = queue.popleft()
 
-            colors[u] = color
-            for v in graph[u]:
-                if not bipartite(v, -color):
-                    return False
+                if u in colors:
+                    if colors[u] != color:
+                        return False
+                    continue
+
+                colors[u] = color
+                for v in graph[u]:
+                    queue.append((v, -color))
+
             return True
 
         for u in range(len(graph)):
             if u not in colors:
-                if not bipartite(u, 1):
+                if not bipartite(u):
                     return False
         return True
