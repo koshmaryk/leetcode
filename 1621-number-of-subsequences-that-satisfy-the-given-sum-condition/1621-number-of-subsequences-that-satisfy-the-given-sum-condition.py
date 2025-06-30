542cf77485d3d2144fe87ea3db767c11779d1ac6
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-06-30 13:05:06 +0300

    Time: 6041 ms (30.76%), Space: 28.2 MB (51.52%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -3,11 +3,18 @@ class Solution:
         nums.sort()
 
         ans = 0
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            if nums[l] + nums[r] <= target:
-                ans += 2**(r - l) # l is min and any of [l+1, r] is max and can be included or excluded, so 2^(r - l)
-                l += 1
-            else:
-                r -= 1
+        for l in range(len(nums)):
+            if nums[l] * 2 > target:
+                break
+
+            bad, good = l, len(nums)
+            while good - bad > 1:
+                mid = (bad + good) // 2
+                if nums[l] + nums[mid] > target:
+                    good = mid
+                else:
+                    bad = mid
+            
+            ans += 2**(good - 1 - l)
+
         return ans % (10**9 + 7)
