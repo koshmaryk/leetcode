4775bdbc9018d871323e31b5332934286403ae7d
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-01 23:24:29 +0300

    Time: 13 ms (38.73%), Space: 18.1 MB (60.76%) - LeetHub

--- 3825-apply-substitutions/3825-apply-substitutions.py
+++ 3825-apply-substitutions/3825-apply-substitutions.py
@@ -4,37 +4,37 @@ import heapq
 class Solution:
     # A, B -> C 
     def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
-        lookup = {key:val for key,val in replacements}
-        
-        graph = defaultdict(set)
-        in_degree = defaultdict(int)
+        lookup = {key:val for key,val in replacements} # N
+
+        graph = defaultdict(set) # N
+        in_degree = defaultdict(int) # N
         for key,val in replacements:
-            for other_key,_ in replacements:
-                if f"%{other_key}%" in val:
+            for other_key,_ in replacements: # N^2
+                if f"%{other_key}%" in val: # MaxStringLength * MaxKeyLength
                     graph[other_key].add(key)
                     in_degree[key] += 1
 
-        pq = [u for u in lookup if in_degree[u] == 0]
-        heapq.heapify(pq)
+        queue = deque([u for u in lookup if in_degree[u] == 0]) # N
 
         toposort = []
-        while pq:
-            u = heapq.heappop(pq)
+        # N + D
+        while queue:
+            u = queue.popleft()
             toposort.append(u)
 
             for v in graph[u]:
                 in_degree[v] -= 1
                 if in_degree[v] == 0:
-                    heapq.heappush(pq, v)
+                    queue.append(v)
         
         #  0  1  2
         # [A, B, C]
         for i in range(len(toposort)):
             key, val = toposort[i], lookup[toposort[i]]
-            for j in range(i):
+            for j in range(i): # N^2
                 prev_key, prev_val = toposort[j], lookup[toposort[j]]
                 if key in graph[prev_key]:
-                    val = val.replace(f"%{prev_key}%", prev_val)
+                    val = val.replace(f"%{prev_key}%", prev_val) # MaxStringLength * MaxKeyLength
             lookup[key] = val
-            text = text.replace(f"%{key}%", val)
+            text = text.replace(f"%{key}%", val) # MaxStringLength * MaxKeyLength
         return text
