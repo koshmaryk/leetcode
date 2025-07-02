88fde3b850751ef757783596733639eeb2dfebef
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-03 01:15:49 +0300

    Time: 5 ms (65.91%), Space: 18.7 MB (95.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,16 +1,32 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x: int):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x: int, y: int):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if self.size[x_root] > self.size[y_root]:
+            self.parent[y_root] = x_root
+            self.size[x_root] += self.size[y_root]
+        else:
+            self.parent[x_root] = y_root
+            self.size[y_root] += self.size[x_root]
+        
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(u):
-            visited[u] = True
-            for v in range(len(isConnected)):
-                if isConnected[u][v] and not visited[v]:
-                    dfs(v)
-
-        visited = [False] * len(isConnected)
-
-        count = 0
-        for u in range(len(isConnected)):
-            if not visited[u]:
-                count += 1
-                dfs(u)
+        n = len(isConnected)
+        uf = UnionFind(n)
+        count = n
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] and uf.find(i) != uf.find(j):
+                    uf.union(i, j)
+                    count -= 1
         return count
