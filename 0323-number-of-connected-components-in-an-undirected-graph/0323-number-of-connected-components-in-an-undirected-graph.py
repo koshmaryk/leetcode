139926ac1d17846b59a11fc9c3f099bc69ca7ff5
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-03 01:26:41 +0300

    Time: 17 ms (5.44%), Space: 19.1 MB (49.45%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,4 +1,25 @@
-from collections import deque, defaultdict
+from collections import defaultdict
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x: int):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x: int, y: int):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if self.size[x_root] > self.size[y_root]:
+            self.parent[y_root] = x_root
+            self.size[x_root] += self.size[y_root]
+        else:
+            self.parent[x_root] = y_root
+            self.size[y_root] += self.size[x_root]
+
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
@@ -7,17 +28,13 @@ class Solution:
             graph[a].append(b)
             graph[b].append(a)
 
-        def dfs(u):
-            visited[u] = True
-            for v in graph[u]:
-                if not visited[v]:
-                    dfs(v)
+        uf = UnionFind(n)
 
-        visited = [False] * n
-
-        count = 0
+        count = n
         for u in range(n):
-            if not visited[u]:
-                count += 1
-                dfs(u)
+            for v in graph[u]:
+                if uf.find(u) != uf.find(v):
+                    uf.union(u, v)
+                    count -= 1
         return count
+        
