cb6db70cded25291b955528a9253f82883a92942
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-05 17:52:20 +0300

    Time: 7 ms (27.47%), Space: 19.3 MB (40.89%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for a,b in prerequisites:
+            graph[b].append(a)
+        
+        ts = []
+        colors = [-1] * numCourses
+        def isCycle(u):
+            if colors[u] == 0:
+                return True
+            if colors[u] == 1:
+                return False
+
+            colors[u] = 0
+            for v in graph[u]:
+                if isCycle(v):
+                    return True
+            colors[u] = 1
+            ts.append(u)
+
+
+        for u in range(numCourses):
+            if colors[u] == -1:
+                if isCycle(u):
+                    return []
+        return ts[::-1]
