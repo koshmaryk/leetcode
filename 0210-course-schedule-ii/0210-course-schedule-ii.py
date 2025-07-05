04977d3f00118cb879180e51e7547a7b72db786c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-05 18:06:47 +0300

    Time: 0 ms (100%), Space: 18.9 MB (85.21%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,32 +1,21 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
+        in_degree = defaultdict(int)
         for a,b in prerequisites:
             graph[b].append(a)
-        
+            in_degree[a] += 1
+
         ts = []
-        colors = [-1] * numCourses
-        def isCycle(u):
-            if colors[u] == 0:
-                return True
-            if colors[u] == 1:
-                return False
-
-            colors[u] = 0
-            for v in graph[u]:
-                if isCycle(v):
-                    return True
-            colors[u] = 1
+        queue = deque([v for v in range(numCourses) if in_degree[v] == 0])
+        while queue:
+            u = queue.popleft()
             ts.append(u)
+            for v in graph[u]:
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    queue.append(v)
 
-
-        for u in range(numCourses):
-            if colors[u] == -1:
-                if isCycle(u):
-                    return []
-        ans = []
-        while ts:
-            ans.append(ts.pop())
-        return ans
+        return ts if len(ts) == numCourses else []
