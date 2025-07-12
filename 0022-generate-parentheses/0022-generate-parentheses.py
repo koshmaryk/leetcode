1fced387baffcc138fdd1f8cc412697aa8fad559
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-12 21:23:26 +0300

    Time: 34 ms (5.17%), Space: 18.1 MB (43.32%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def valid(s: str):
+            count = 0
+            for c in s:
+                count += 1 if c == "(" else -1
+                if count < 0:
+                    return False
+            return count == 0
+
+        def dfs(s: str):
+            if len(s) == 2 * n:
+                if valid(s):
+                    output.append(s)
+                return
+            
+            dfs(s + "(")
+            dfs(s + ")")
+
+        output = []
+        dfs("")
+        return output
