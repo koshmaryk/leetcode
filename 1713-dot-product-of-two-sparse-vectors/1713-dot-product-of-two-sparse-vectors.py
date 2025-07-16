e6dbaa253856e014e3f9712c89c334b361a43fce
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-17 00:43:54 +0300

    Time: 1344 ms (28.7%), Space: 22.3 MB (16.18%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
@@ -1,37 +1,36 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
         self.nonzero = []
-        self.indices = []
-        for i, val in enumerate(nums):
-            if val != 0:
-                self.indices.append(i)
-                self.nonzero.append(val)
+        for i, n in enumerate(nums):
+            if n != 0:
+                self.nonzero.append((i, n))
         
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
-        if len(self.indices) < len(vec.indices):
+        if len(self.nonzero) < len(vec.nonzero):
             smaller, larger = self, vec
         else:
             smaller, larger = vec, self
 
         result = 0
-        for i, idx in enumerate(smaller.indices):
-            pos = self.binary_search(larger.indices, idx)
+        for index, pair in enumerate(smaller.nonzero):
+            i, n = pair
+            pos = self.binary_search(larger.nonzero, i)
             if pos != -1:
-                result += smaller.nonzero[i] * larger.nonzero[pos]
+                result += smaller.nonzero[index][1] * larger.nonzero[pos][1]
         
         return result
 
-    def binary_search(self, arr: List[int], target: int) -> int:
+    def binary_search(self, arr: List[tuple[int, int]], target: int) -> int:
         bad, good = -1, len(arr)
         while good - bad > 1:
             mid = (bad + good) // 2
-            if arr[mid] >= target:
+            if arr[mid][0] >= target:
                 good = mid
             else:
                 bad = mid
-        return -1 if good == len(arr) or arr[good] != target else good
+        return -1 if good == len(arr) or arr[good][0] != target else good
 
 
 # Your SparseVector object will be instantiated and called as such:
