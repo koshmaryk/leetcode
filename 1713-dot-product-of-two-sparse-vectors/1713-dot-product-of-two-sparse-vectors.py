dcd8da09784ba53632656aaf6ff9f3877a58b6b2
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-17 00:49:03 +0300

    Time: 1324 ms (52.3%), Space: 22.1 MB (24.26%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
@@ -6,7 +6,6 @@ class SparseVector:
                 self.nonzero.append((i, n))
         
 
-    # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
         if len(self.nonzero) < len(vec.nonzero):
             smaller, larger = self, vec
@@ -14,15 +13,14 @@ class SparseVector:
             smaller, larger = vec, self
 
         result = 0
-        for index, pair in enumerate(smaller.nonzero):
-            i, n = pair
-            pos = self.binary_search(larger.nonzero, i)
-            if pos != -1:
-                result += n * larger.nonzero[pos][1]
+        for i, n in smaller.nonzero:
+            j = self.binary_search(larger.nonzero, i)
+            if j != -1:
+                result += n * larger.nonzero[j][1]
         
         return result
 
-    def binary_search(self, arr: List[tuple[int, int]], target: int) -> int:
+    def binary_search(self, arr: List[Tuple[int, int]], target: int) -> int:
         bad, good = -1, len(arr)
         while good - bad > 1:
             mid = (bad + good) // 2
