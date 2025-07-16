3b63a34cf1c53bdeea8c23dd5c910c98ee0ebf5b
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-17 00:34:40 +0300

    Time: 1332 ms (41.09%), Space: 21.6 MB (81.69%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
@@ -1,26 +1,38 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
         self.nonzero = []
-        for i, n in enumerate(nums):
-            if n != 0:
-                self.nonzero.append((i, n))
+        self.indices = []
+        for i, val in enumerate(nums):
+            if val != 0:
+                self.indices.append(i)
+                self.nonzero.append(val)
         
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
+        if len(self.indices) < len(vec.indices):
+            smaller, larger = self, vec
+        else:
+            smaller, larger = vec, self
+
         result = 0
-        p, q = 0, 0
-        while p < len(self.nonzero) and q < len(vec.nonzero):
-            if self.nonzero[p][0] < vec.nonzero[q][0]:
-                p += 1
-            elif self.nonzero[p][0] > vec.nonzero[q][0]:
-                q += 1
-            else:
-                result += self.nonzero[p][1] * vec.nonzero[q][1]
-                p += 1
-                q += 1
-        return result
+        for i, idx in enumerate(smaller.indices):
+            pos = self.binary_search(larger.indices, idx)
+            if pos != -1:
+                result += smaller.nonzero[i] * larger.nonzero[pos]
         
+        return result
+
+    def binary_search(self, arr: List[int], target: int) -> int:
+        bad, good = -1, len(arr)
+        while good - bad > 1:
+            mid = (bad + good) // 2
+            if arr[mid] >= target:
+                good = mid
+            else:
+                bad = mid
+        return -1 if good == len(arr) or arr[good] != target else good
+
 
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
