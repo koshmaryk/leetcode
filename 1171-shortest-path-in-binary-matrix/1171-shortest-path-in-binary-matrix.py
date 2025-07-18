ed7c8219363f35d5482b4e9c0005ad43a73af843
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-18 23:31:01 +0300

    Time: 158 ms (74.64%), Space: 19.3 MB (50.47%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -6,21 +6,22 @@ class Solution:
         if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1
 
-        def heuristic(r, c):
-            return max(abs(r - n - 1), abs(c - n - 1))
+        def estimate(r, c):
+            return max(n - 1 - r, n - 1 - c)
 
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, 1), (1, -1), (-1, 1)]
         visited = set()
-        pq = [(1 + heuristic(0, 0), 1, 0, 0)]
+        pq = [(1 + estimate(0, 0), 1, 0, 0)]
         while pq:
-            score, dist, r, c = heapq.heappop(pq)
+            est, dist, r, c = heapq.heappop(pq)
             if (r, c) in visited:
                 continue
             if r == n - 1 and c == n - 1:
                 return dist
+
             visited.add((r, c))
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < n and 0 <= nc < n and (nr, nc) not in visited and grid[nr][nc] == 0:
-                    heapq.heappush(pq, (dist + 1 + heuristic(nr, nc), dist + 1, nr, nc))
+                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
+                    heapq.heappush(pq, (dist + 1 + estimate(nr, nc), dist + 1, nr, nc))
         return -1
