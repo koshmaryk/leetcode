275ec31aba83bf99113e873ddd70c7549c3ce662
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-19 02:30:43 +0300

    Time: 23 ms (51.8%), Space: 21.3 MB (52.78%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,35 +1,32 @@
+import heapq
+
 class Solution:
     '''
         [1, 5, 9 ],
         [10,11,13],
         [12,13,15]
 
-
-        k = 2
-        [1,4],
-        [2,5]
+        pq = [13]
+        k = 1
     '''
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
 
-        def countLessThanOrEqual(target: int): # 4
-            count = 0 # 3
-            r, c = 0, n - 1
-            while r < n and c >= 0:
-                if matrix[r][c] <= target:
-                    count += (c + 1)
-                    r += 1
-                else:
-                    c -= 1
-            return count
+        pq = []
+        for r in range(min(k, n)):
+            pq.append((matrix[r][0], r, 0))
 
+        heapq.heapify(pq)
 
-        bad, good = matrix[0][0] - 1, matrix[n-1][n-1] + 1
-        while good - bad > 1: # 5 | 4
-            mid = (bad + good) // 2 # 1 | 1
-            if countLessThanOrEqual(mid) >= k: # matrix[mid // n][mid % n]
-                good = mid
-            else:
-                bad = mid
-        return good #matrix[good // n][good % n]
+        element = pq[0][0]
+        while k:
+            element, r, c = heapq.heappop(pq)
+
+            if c < n - 1:
+                heapq.heappush(pq, (matrix[r][c + 1], r, c + 1))
+
+            k -= 1
+
+        return element
         
+        # if x = min(k, n), then time complexity O(x log x) and space complexity O(x)
