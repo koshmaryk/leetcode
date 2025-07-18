7787039a89b50fd5d72c6955ebf76f042a59e90d
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-19 02:51:03 +0300

    Time: 22 ms (52.51%), Space: 22 MB (12.94%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,32 +1,14 @@
 import heapq
 
 class Solution:
-    '''
-        [1, 5, 9 ],
-        [10,11,13],
-        [12,13,15]
-
-        pq = [13]
-        k = 1
-    '''
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
-
         pq = []
-        for r in range(min(k, n)):
-            pq.append((matrix[r][0], r, 0))
+        for r in range(n):
+            for c in range(n):
+                heapq.heappush(pq, -matrix[r][c])
 
-        heapq.heapify(pq)
-
-        element = pq[0][0]
-        while k:
-            element, r, c = heapq.heappop(pq)
-
-            if c < n - 1:
-                heapq.heappush(pq, (matrix[r][c + 1], r, c + 1))
-
-            k -= 1
-
-        return element
+                if len(pq) > k:
+                    heapq.heappop(pq)
+        return -heapq.heappop(pq)
         
-        # if x = min(k, n), then time complexity O(x log x) and space complexity O(x)
