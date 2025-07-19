c3b9624daeb4044619a32c12dccfc9eaba108016
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-19 23:39:25 +0300

    Time: 87 ms (31.92%), Space: 21 MB (16.76%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.py
+++ 0971-shortest-bridge/0971-shortest-bridge.py
@@ -10,14 +10,15 @@ class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         n = len(grid)
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        island = set()
         queue = deque([])
 
         def dfs(r, c):
-            grid[r][c] = 2
+            island.add((r, c))
             queue.append((r, c))
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 1:
+                if 0 <= nr < n and 0 <= nc < n and (nr, nc) not in island and grid[nr][nc] == 1:
                     dfs(nr, nc)
 
         i, j = 0, 0
@@ -29,20 +30,24 @@ class Solution:
 
         dfs(i, j)
 
+        water = set()
         distance = 0
         while queue:
             size = len(queue)
             for _ in range(size):
                 r, c = queue.popleft()
+                if (r, c) in water:
+                    continue
+
+                water.add((r, c))
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
                     if  0 <= nr < n and 0 <= nc < n:
-                        if grid[nr][nc] == 1:
+                        if grid[nr][nc] == 1 and (nr, nc) not in island:
                             return distance
 
                         if grid[nr][nc] == 0:
-                            grid[nr][nc] = -1
                             queue.append((nr, nc))
 
             distance += 1
-        return distance
+        return -1
