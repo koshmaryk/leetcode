2048581a97c1e337bf7e9591f5fa13fd3527937b
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-19 17:34:50 +0300

    Time: 3 ms (97.35%), Space: 19.8 MB (60.37%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -2,31 +2,42 @@ class Solution:
     '''
         [1,2,4,7,5,3,6,8,9]
 
-
         [1,2,3],
         [4,5,6],
         [7,8,9]
 
-
         5
         0= 0,0; 1=0,1; 2=0,2; 3=1,2; 4=2,2
 
+        0,0 0,0
+        0,1 1,0
+        2,0 0,2
+        1,2 2,1
+        2,2 2,2
     '''
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         output = []
         m, n = len(mat), len(mat[0])
-        for i in range(m + n - 1):
-            diagonal = []
-
-            r, c = 0 if i < n else i - n + 1, i if i < n else n - 1
-            while r < m and c > -1:
-                diagonal.append(mat[r][c])
-                r += 1
-                c -= 1
-
-            if i % 2 == 0:
-                output.extend(diagonal[::-1])
+        arrow = 1
+        r, c = 0, 0
+        for i in range(m * n):
+            output.append(mat[r][c])
+            
+            if (r + c) % 2 == 0:
+                if c == n - 1:
+                    r += 1
+                elif r == 0:
+                    c += 1
+                else:
+                    r -= 1
+                    c += 1
             else:
-                output.extend(diagonal)
+                if r == m - 1:
+                    c += 1
+                elif c == 0:
+                    r += 1
+                else:
+                    r += 1
+                    c -= 1
         return output
         
