ffdd7c819f51ccc3f9ba360f132434d030e5c2b6
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-19 21:48:12 +0300

    Time: 55 ms (76.57%), Space: 19.2 MB (59.86%) - LeetHub

---
+++ 0971-shortest-bridge/0971-shortest-bridge.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+class Solution:
+    '''
+        [0,1,0],
+        [0,0,0],
+        [0,0,1]
+
+    '''
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        queue = deque([])
+
+        def dfs(r, c):
+            grid[r][c] = 2
+            queue.append((r, c))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 1:
+                    dfs(nr, nc)
+
+        i, j = 0, 0
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] == 1:
+                    i, j = r, c
+
+        dfs(i, j)
+
+        distance = 0
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if  0 <= nr < n and 0 <= nc < n:
+                        if grid[nr][nc] == 1:
+                            return distance
+
+                        if grid[nr][nc] == 0:
+                            grid[nr][nc] = -1
+                            queue.append((nr, nc))
+
+            distance += 1
+        return distance
