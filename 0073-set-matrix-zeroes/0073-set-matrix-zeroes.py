7dabeeff36b53b05c5d01534f7a54f14951a3f21
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-20 18:57:02 +0300

    Time: 0 ms (100%), Space: 18.6 MB (35.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,7 +1,18 @@
 class Solution:
     '''
+        rows [0,0,0,0]
+        cols [0,0,0]
+
+        [1,1,2,0],
+        [1,4,0,2],
+        [1,3,1,5]
+
         [0,1,2,0],
         [3,4,5,2],
+        [1,3,1,5]
+
+        [0,1,0,0],
+        [0,4,0,2],
         [1,3,1,5]
 
 
@@ -15,20 +26,20 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         m, n = len(matrix), len(matrix[0])
-        zero_1st_col = False
-        
+        zero_col = False
+
         for r in range(m):
             if matrix[r][0] == 0:
-                zero_1st_col = True
+                zero_col = True
             for c in range(1, n):
                 if matrix[r][c] == 0:
                     matrix[r][0] = 0
                     matrix[0][c] = 0
-
+        
         for r in range(m - 1, -1, -1):
             for c in range(n - 1, 0, -1):
                 if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
 
-            if zero_1st_col:
+            if zero_col:
                 matrix[r][0] = 0
