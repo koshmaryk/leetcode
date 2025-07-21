f9b9508271e176d7d3e6be509f1b0853982945fb
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-21 20:30:39 +0300

    Time: 96 ms (46.38%), Space: 35.5 MB (33.83%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,36 +1,19 @@
 class Solution:
-    '''
-        nums 1,3,1,2,2
-    min_nums 1 1 1 1 1
-
-
-        nums 3,1,4,2,0
-
-    min_nums 3 1 1 1 0
-
-stack = [2]       
-    '''
     def find132pattern(self, nums: List[int]) -> bool:
         n = len(nums)
-        if n < 3:
-            return False
-    
-        min_nums = [0] * n
-        min_nums[0] = nums[0]
-        for i in range(1, n):
-            min_nums[i] = min(min_nums[i - 1], nums[i])
+        min_i = [0] * n
+        min_i[0] = nums[0]
+        for j in range(1, n):
+            min_i[j] = min(min_i[j - 1], nums[j])
 
         stack = []
         for j in range(n - 1, -1, -1):
-            if nums[j] <= min_nums[j]:
-                continue
+            if nums[j] > min_i[j]:
+                while stack and stack[-1] <= min_i[j]: # nums[k] > nums[i]
+                    stack.pop()
 
-            while stack and stack[-1] <= min_nums[j]: # nums[j] > minimum for nums[j]
-                stack.pop()
+                if stack and stack[-1] < nums[j]:      # nums[k] < nums[j]
+                    return True
 
-            while stack and stack[-1] < nums[j]: # nums[k] < nums[j]
-                return True
-
-            stack.append(nums[j])
-        
+                stack.append(nums[j])
         return False
