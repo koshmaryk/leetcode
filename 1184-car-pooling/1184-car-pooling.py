2de548478f31886d04eb96c1751e727d12b425a3
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-07-22 21:51:51 +0300

    Time: 3 ms (63.2%), Space: 18 MB (99.1%) - LeetHub

---
+++ 1184-car-pooling/1184-car-pooling.py
@@ -0,0 +1,25 @@
+import heapq
+
+'''
+    capacity = 3
+    [[2,1,5],[3,5,7]]
+
+'''
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda x:x[1])
+
+        pq = []
+        total = 0
+        for i in range(len(trips)):
+            passengers, pickup, dropoff = trips[i]
+            while pq and pq[0][0] <= pickup:
+                total -= trips[heapq.heappop(pq)[1]][0]
+
+            total += passengers
+            if total > capacity:
+                return False
+
+            heapq.heappush(pq, (dropoff, i))
+
+        return True
