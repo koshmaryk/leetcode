387ce67b1f4dd3bf927e6b3a62800a84124e2a3d
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-08-03 15:08:47 +0300

    Time: 0 ms (100%), Space: 17.9 MB (8.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -8,7 +8,7 @@ class Solution:
 
         k = 2
         for i in range(1, n + 1):
-            for j in range(1, k + 1):
+            for j in range(1, min(i, k) + 1):
                 dp[i] += dp[i - j]
         return dp[n]
         
