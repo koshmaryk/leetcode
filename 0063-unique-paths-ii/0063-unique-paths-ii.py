6fb486458c00d041fbb48aeb4eff0c89151fcc25
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-08-04 00:33:18 +0300

    Time: 0 ms (100%), Space: 18.1 MB (30.51%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,22 +1,24 @@
+from collections import defaultdict
+
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
-        if grid[0][0] == 1:
-            return 0
-    
-        grid[0][0] = 1
-        
-        for i in range(1, m):
-            grid[i][0] = int(grid[i - 1][0] == 1 and grid[i][0] == 0)
+        def dfs(i, j):
+            if grid[i][j] == 1:
+                return 0
 
-        for j in range(1, n):
-            grid[0][j] = int(grid[0][j - 1] == 1 and grid[0][j] == 0)
+            if i == m - 1 and j == n - 1:
+                return 1
 
-        for i in range(1, m):
-            for j in range(1, n):
-                if grid[i][j] == 1:
-                    grid[i][j] = 0
-                else:
-                    grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
-        return grid[m - 1][n - 1]
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if i < m - 1:
+                memo[(i, j)] += dfs(i + 1, j)
+            if j < n - 1:
+                memo[(i, j)] += dfs(i, j + 1)
+            return memo[(i, j)]
+
+        memo = defaultdict(int)
+        return dfs(0, 0)
