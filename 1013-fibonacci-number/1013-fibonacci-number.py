b2ef5f7750f57b4869d18d26fe3b3254a32a6c63
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-08-03 12:29:48 +0300

    Time: 26 ms (98.57%), Space: 17.9 MB (23.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,14 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def f(n):
-            if n < 2:
-                return n
+        if n < 2:
+            return n
 
-            if n in memo:
-                return memo[n]
-
-            memo[n] = f(n - 1) + f(n - 2)
-            return memo[n]
-
-        memo = {}
-        return f(n)
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+        
