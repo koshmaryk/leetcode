2b7952af0f9450915ce52cc40a45e10807f2cb71
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-08-04 10:21:36 +0300

    Time: 0 ms (100%), Space: 17.7 MB (78.51%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,15 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
-        dp = [0] * (n + 1)
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 1
+        def f(n):
+            if n == 0:
+                return 0
+            if n == 1 or n == 2:
+                return 1
 
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-        return dp[n]
+            if n in memo:
+                return memo[n]
+
+            memo[n] = f(n - 1) + f(n - 2) + f(n - 3)
+            return memo[n]
+
+        memo = {}
+        return f(n)
         
