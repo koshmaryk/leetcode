cf565c3f748bb8a41ec6f31da4e546110aca8395
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-08-06 21:43:10 +0300

    Time: 1773 ms (41.11%), Space: 18 MB (55.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,11 +4,9 @@ class Solution:
         ans = 0
         dp = [1] * n
         for i in range(n):
-            curr_max = 0
             for j in range(i):
                 if nums[j] < nums[i]:
-                    curr_max = max(curr_max, dp[j])
-            dp[i] = 1 + curr_max
+                    dp[i] = max(dp[i], dp[j] + 1)
             ans = max(ans, dp[i])
         return ans
         
