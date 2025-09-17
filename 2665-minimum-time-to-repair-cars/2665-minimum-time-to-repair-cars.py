5c89cd1047f9b35c3f6f3ac9112f64bfdf017afb
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-17 19:50:41 +0300

    Time: 1115 ms (17.76%), Space: 22 MB (55.36%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -0,0 +1,17 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def isGoodEnough(t):
+            repaired = 0
+            for rank in ranks:
+                repaired += int((t / rank) ** 0.5)
+            return repaired >= cars
+
+        bad, good = 0, max(ranks) * cars ** 2
+        while good - bad > 1:
+            guess = (bad + good) // 2
+            if isGoodEnough(guess):
+                good = guess
+            else:
+                bad = guess
+        return good
+
