afd85dc775ac776510cd282c00508e77a7f66ab8
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-20 19:27:03 +0300

    Time: 3 ms (78.4%), Space: 21.3 MB (51.33%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -6,14 +6,30 @@ class Solution:
     [10,11,13]
     [12,13,15]
 
+    (0, 16)
+
+    13
+
     '''
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        pq = []
         n = len(matrix)
-        for r in range(n):
-            for c in range(n):
-                heapq.heappush(pq, -matrix[r][c])
 
-                if len(pq) > k:
-                    heapq.heappop(pq)
-        return -pq[0]
+        def isGoodEnough(target):
+            cnt = 0
+            r, c = 0, n - 1
+            while r < n and c >= 0:
+                if matrix[r][c] <= target:
+                    cnt += c + 1
+                    r += 1
+                else:
+                    c -= 1
+            return cnt >= k
+
+        bad, good = matrix[0][0] - 1, matrix[n - 1][n - 1]
+        while good - bad > 1:
+            guess = (bad + good) // 2
+            if isGoodEnough(guess):
+                good = guess
+            else:
+                bad = guess
+        return good
