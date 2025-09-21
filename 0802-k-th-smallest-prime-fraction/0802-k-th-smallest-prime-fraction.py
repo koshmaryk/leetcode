143933d4694f8b4a108103dfea550c42ea6696e9
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-21 13:14:22 +0300

    Time: 4141 ms (5.02%), Space: 18.1 MB (66.61%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.py
@@ -1,14 +1,39 @@
 class Solution:
+    '''
+
+    1,2,3,5
+
+
+    '''
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         n = len(arr)
-        
-        hashmap = {}
-        fractions = []
-        for i in range(n):
-            for j in range(n):
-                fraction = arr[i] / arr[j]
-                fractions.append(fraction)
-                hashmap[fraction] = [arr[i], arr[j]]
-        
-        fractions.sort()
-        return hashmap[fractions[k - 1]]
+
+        def count(f):
+            max_f = 0.0
+            args = []
+            cnt = 0
+            for i in range(n):
+                j = 1
+                while j < n and arr[i] >= f * arr[j]:
+                    j += 1
+
+                cnt += n - j
+
+                if j < n and max_f < arr[i] / arr[j]:
+                    max_f = arr[i] / arr[j]
+                    args = [arr[i], arr[j]]
+            return cnt, args
+
+        answer = []
+
+        bad, good = 0.0, 1.0
+        while good - bad > 0.000000001:
+            guess = (bad + good) / 2
+            cnt, answer = count(guess)
+            if cnt > k:
+                good = guess
+            elif cnt < k:
+                bad = guess
+            else:
+                return answer
+        return answer
