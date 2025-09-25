6c8f280fdeb18dcb1be594e2928977707c01746a
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-25 20:30:47 +0300

    Time: 99 ms (31.67%), Space: 18.4 MB (32.63%) - LeetHub

---
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numFriendRequests(self, ages: List[int]) -> int:
+        count = [0] * 121
+        for age in ages:
+            count[age] += 1
+
+        ans = 0
+        for x in range(1, 121):
+            for y in range(1, 121):
+                if y <= 0.5 * x + 7:
+                    continue
+                if y > x:
+                    continue
+                if y > 100 and x < 100:
+                    continue
+
+                ans += count[x] * count[y]
+                if x == y:
+                    ans -= count[x]
+        return ans
