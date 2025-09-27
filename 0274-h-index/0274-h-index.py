d882e518479c349f2f5fba82df722afada1b9257
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-28 01:01:28 +0300

    Time: 0 ms (100%), Space: 18.3 MB (20.07%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -2,13 +2,18 @@ class Solution:
     ''' 
     3,0,6,1,5
 
-    6,5,3,1,0
+    1,1,0,1,0,2
 
     '''
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True) # 6,5,3,1,0
-        h = 0
-        for i, citation in enumerate(citations): # 0,6 | 1,5 | 2,3 | 3,1 | ... 
-            if citation >= i + 1:
-                h = i + 1 # 1 | 2 | 3 | 3 ...
-        return h
+        n = len(citations)
+        count = [0] * (n + 1)
+        for citation in citations:
+            count[min(citation, n)] += 1
+
+        p = 0
+        for h in range(n, -1, -1):
+            p += count[h]
+            if p >= h:
+                return h
+        return 0
