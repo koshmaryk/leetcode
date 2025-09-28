a203292eaab3fcb486ab452c5e51b1b83cc20aed
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-28 14:08:54 +0300

    Time: 0 ms (100%), Space: 17.7 MB (84.09%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -2,20 +2,15 @@ class Solution:
     '''
     "2[abc]3[cd]ef"
 
-    stack = abcabc 3
-
-    abcabccdcdcdef
-    
-    curr_s = abcabccdcdcdef
-    curr_k = 0
+    stack = []
 
     Time Complexity: maxK ^ countK * maxEncodedStringLength
     Space Complexity: maxK ^ countK * maxEncodedStringLength
     '''
     def decodeString(self, s: str) -> str:
+        stack = []
         curr_s = ""
         curr_k = 0
-        stack = []
         for c in s:
             if c.isdigit():
                 curr_k = curr_k * 10 + int(c)
