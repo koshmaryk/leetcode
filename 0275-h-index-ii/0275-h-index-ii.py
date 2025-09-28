d6e744bcd4d6d2aba18fcfe2cdbd5fcfb9e1af12
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-09-28 23:50:03 +0300

    Time: 0 ms (100%), Space: 22.7 MB (99.27%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -2,15 +2,19 @@ class Solution:
     '''
     0 1 2 3 4
 
-    0,1,3,5,6
+    0,1,1,2,3,5,6
 
-    n=5; 
+    n=7; 
 
 
     '''
     def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
-        for i, citation in enumerate(citations):
-            if citation >= n - i:
-                return n - i
-        return 0
+        bad, good = -1, n
+        while good - bad > 1:
+            mid = (bad + good) // 2
+            if citations[mid] >= n - mid:
+                good = mid
+            else:
+                bad = mid
+        return n - good
