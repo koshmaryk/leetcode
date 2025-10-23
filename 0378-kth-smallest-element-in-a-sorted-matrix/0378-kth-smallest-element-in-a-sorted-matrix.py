b21f4e66bcc57c26f398cc88edba129e9b78b684
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-10-23 12:46:02 +0300

    Time: 0 ms (100%), Space: 21.4 MB (28.07%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,26 +1,32 @@
-import heapq
-
 class Solution:
     '''
     [1, 5, 9 ]
     [10,11,13]
     [12,13,15]
 
-    k = 8
+    (0, 16)
+
+    13
 
     '''
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        def countLessOrEqual(num): # 13
+            cnt = 0
+            r, c = 0, n - 1 # 0,2 | 1,2 | 2,2 | 2,1 | 3,1
+            while r < n and c >= 0:
+                if matrix[r][c] <= num:
+                    cnt += c + 1 # 0+3+3+2
+                    r += 1
+                else:
+                    c -= 1
+            return cnt
+
         n = len(matrix)
-        minHeap = []
-        for r in range(min(k, n)):
-            minHeap.append((matrix[r][0], r, 0))
-
-        heapq.heapify(minHeap)
-
-        ans = float('inf')
-        for i in range(k):
-            ans, r, c = heapq.heappop(minHeap)
-            if c + 1 < n:
-                heapq.heappush(minHeap, (matrix[r][c + 1], r, c + 1))
-        return ans
-
+        bad, good = matrix[0][0] - 1, matrix[n - 1][n - 1] + 1 # 0,16 | 8,16 | 12,16 | 12,14 | 12,13
+        while good - bad > 1:
+            guess = (bad + good) // 2 # 8 | 12 | 14 | 13
+            if countLessOrEqual(guess) >= k:
+                good = guess
+            else:
+                bad = guess
+        return good
