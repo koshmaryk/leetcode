3fb84d7ae10815814ee96e5b99763eba09d01784
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-01 16:33:22 +0200

    Time: 0 ms (100%), Space: 18 MB (16.87%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -1,19 +1,19 @@
 class Solution:
-    '''
-    
-
-    '''
     def numWays(self, n: int, k: int) -> int:
-        if n == 1:
-            return k
+        memo = {}
 
-        if n == 2:
-            return k * k
+        def totalWays(i):
+            if i == 1:
+                return k
 
-        two_back = k
-        one_back = k * k
+            if i == 2:
+                return k * k
 
-        for i in range(3, n + 1):
-            curr = (k - 1) * (two_back + one_back)
-            two_back, one_back = one_back, curr
-        return one_back
+            if i in memo:
+                return memo[i]
+
+            memo[i] = (k - 1) * (totalWays(i - 2) + totalWays(i - 1))
+            return memo[i]
+
+        return totalWays(n)
+        
