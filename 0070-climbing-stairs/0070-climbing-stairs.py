a5bb768d7de7eecaa4aff735882a243d90667b2b
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-01 19:59:52 +0200

    Time: 0 ms (100%), Space: 17.8 MB (48.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,17 +3,17 @@ class Solution:
         memo = {}
 
         def count(i):
-            if i == 0:
-                return 1
+            if i > n:
+                return 0
 
-            if i == 1:
+            if i == n:
                 return 1
 
             if i in memo:
                 return memo[i]
 
-            memo[i] = count(i - 1) + count(i - 2)
+            memo[i] = count(i + 1) + count(i + 2)
             return memo[i]
 
-        return count(n)
+        return count(0)
                 
