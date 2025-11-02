85e6bddfbe6b3839f6373c4c16d55bfc73be76da
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-02 14:30:38 +0200

    Time: 0 ms (100%), Space: 17.6 MB (79.77%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -5,6 +5,10 @@ class Solution:
     dp[2] = 2
     dp[3]
 
+    1,2,3,4,5
+    
+    1   2   3,4,5
+
     1; 1->2, 2->1; 3->2->1; 1<-3->2, 1->2->3, 2->3->1, 2->1->3
     '''
     def numTrees(self, n: int) -> int:
@@ -13,6 +17,7 @@ class Solution:
         dp[1] = 1
         for nodes in range(2, n + 1):
             for root in range(1, nodes + 1):
+                # root=2, left=2-1, right=5-2
                 dp[nodes] += dp[root - 1] * dp[nodes - root]
         return dp[n]
         
