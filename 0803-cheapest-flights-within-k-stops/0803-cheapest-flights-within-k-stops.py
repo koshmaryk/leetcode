881a6c2fda430b97be21992026c1a36c6c2c75ee
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-13 00:54:04 +0200

    Time: 0 ms (100%), Space: 19.7 MB (11.67%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -4,27 +4,25 @@ import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = defaultdict(list)
-        for s,d,p in flights:
-            graph[s].append((d,p))
-
-        dist = {}
-        dist[(src, 0)] = 0
-
-        pq = [(0, src, 0)]
+        for s, d, p in flights:
+            graph[s].append((d, p))
+        
+        stops = [float('inf')] * n
+        
+        pq = [(0, 0, src)]
+        
         while pq:
-            price, city, stops = heapq.heappop(pq)
-
+            price, stop, city = heapq.heappop(pq)
+            
+            if stop >= stops[city] or stop > k + 1:
+                continue
+            
+            stops[city] = stop
+            
             if city == dst:
                 return price
-
-            if stops > k:
-                continue
-
+            
             for next_city, next_price in graph[city]:
-                new_price = price + next_price
-                new_stops = stops + 1
-                if (next_city, new_stops) not in dist or new_price < dist[(next_city, new_stops)]:
-                    dist[(next_city, new_stops)] = new_price
-                    heapq.heappush(pq, (new_price, next_city, new_stops))
-        return -1
+                heapq.heappush(pq, (price + next_price, stop + 1, next_city))
         
+        return -1
