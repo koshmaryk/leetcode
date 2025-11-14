e1c81d6e2a5b3f1443bf7f367885e2fd6e27a295
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-15 00:35:22 +0200

    Time: 373 ms (61.64%), Space: 103.7 MB (84.25%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.py
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 import heapq
 
+# TC O(M * Ⲁ(c)), where M = len(connections)
+# SC O(c)
 class UnionFind:
 
     def __init__(self, c):
@@ -19,6 +21,8 @@ class UnionFind:
             else:
                 self.parent[v_root] = u_root
 
+# TC O(c log c + M)
+# SC O(c)
 class Solution:
     def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
         uf = UnionFind(c + 1)
@@ -27,30 +31,32 @@ class Solution:
 
         answer = []
 
-        online = set()
-        groups = {}
-        heaps = defaultdict(list)
+        online = set() # O(c)
+        groups = {} # O(c)
+        heaps = defaultdict(list) # O(c)
 
+        # O(c log c)
         for u in range(c + 1):
-            group_id = uf.find(u)
-            groups[u] = group_id
-            heapq.heappush(heaps[group_id], u)
-            online.add(u)
+            group_id = uf.find(u) # Ⲁ(c)
+            groups[u] = group_id # O(1)
+            heapq.heappush(heaps[group_id], u) # O(log c)
+            online.add(u) # O(1)
 
+        # TC O(c log c)
         for y,x in queries:
             if y == 1:
-                if x in online:
+                if x in online: # O(1)
                     answer.append(x)
                 else:
-                    group = heaps[uf.find(x)]
+                    group = heaps[uf.find(x)] # O(Ⲁ(n))
                     while group and group[0] not in online:
-                        heapq.heappop(group)
+                        heapq.heappop(group) # O(log c)
 
                     if group:
                         answer.append(group[0])
                     else:
                         answer.append(-1)
             else:
-                online.discard(x)
+                online.discard(x) # O(1)
 
         return answer
