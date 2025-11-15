0c60cb206cdf252b045ffcbfa3d2e338538ddd91
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-15 15:01:21 +0200

    Time: 0 ms (100%), Space: 17.7 MB (83.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -8,23 +8,21 @@ from collections import deque
 
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        queue = deque([root])
+        queue = deque([root, root])
         while queue:
-            size = len(queue)
-            tmp = []
-            for i in range(size):
-                node = queue.popleft()
-                tmp.append(node.val if node else float('inf'))
+            t1, t2 = queue.popleft(), queue.popleft()
+            if not t1 and not t2:
+                continue
+            
+            if not t1 or not t2:
+                return False
 
+            if t1.val != t2.val:
+                return False
 
-                if node:
-                    queue.append(node.left)
-                    queue.append(node.right)
-
-            l, r = 0, size - 1
-            while l < r:
-                if tmp[l] != tmp[r]:
-                    return False
-                l += 1
-                r -= 1
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+    
         return True
