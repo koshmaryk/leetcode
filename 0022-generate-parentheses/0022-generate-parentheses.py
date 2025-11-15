aa05145adb085149411b3e218dc70000405e0b24
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-11-15 20:41:18 +0200

    Time: 0 ms (100%), Space: 17.9 MB (69.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 0:
-            return [""]
-
-        # the math property states that every valid () combination can be decomposed to (A)B, 
-        # where A and B are also valid () combination (possibly empty)
-        #               F(n)
-        # ( + F(0) + ) F(n - 1) ... ( + F(n - 1) + ) F(0)
-        #
-        #                              F(3)
-        # ( + F(0) + ) F(2)     ( + F(1) + ) F(1)   ( + F(2) + ) F(0)
-        #       ()(())                 (())()           ((()))
         output = []
-        for opened in range(n):
-            for left_s in self.generateParenthesis(opened):
-                for right_s in self.generateParenthesis(n - 1 - opened):
-                    output.append("(" + left_s + ")" + right_s)
-        return output
+        stack = []
 
-        # Time Complexity: 2n! / ((n + 1)! * n!) or 4^n / √n
-        # C(n) = C(0) * C(n - 1) + C(1) * C(n - 2) + .. + C(n - 1) * C(0)
-        # Space Complexity: 2n
+        def generate(opened, closed):
+            if opened == n and closed == n:
+                output.append("".join(stack))
+                return
+
+            if opened < n:
+                stack.append("(")
+                generate(opened + 1, closed)
+                stack.pop()
+            if closed < opened:
+                stack.append(")")
+                generate(opened, closed + 1)
+                stack.pop()
+
+        generate(0, 0)
+        return output
