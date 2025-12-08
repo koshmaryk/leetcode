3f52af7d0bf10bbe213a221ca668dd8afd86912c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-12-08 22:59:48 +0200

    Time: 202 ms (35.37%), Space: 18.8 MB (45.81%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -3,7 +3,12 @@ import heapq
 '''
     [[10, 20], [20, 30], [40, 50]]
 
-    [60, 75]
+    [10, 20] and [60, 75]
+    [s1, e1] and [s2, e2]
+
+    e1 <= s2 or e2 <= s1
+    e1 > s2 and e2 > s1
+
 '''
 class MyCalendar:
 
@@ -13,7 +18,7 @@ class MyCalendar:
 
     def book(self, startTime: int, endTime: int) -> bool:
         for start,end in self.calendar:
-            if startTime < end and endTime > start:
+            if endTime > start and startTime < end:
                 return False
         self.calendar.append((startTime, endTime))
         return True
