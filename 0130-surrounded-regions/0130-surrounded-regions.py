912bc8426150805a218209e499b450d3897d4396
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2025-12-12 12:40:31 +0200

    Time: 7 ms (56.77%), Space: 21.7 MB (56.16%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -2,47 +2,26 @@ class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
-
-        ["X","X","X","X"]
-        ["X","O","O","X"]
-        ["X","O","O","X"]
-        ["X","O","X","X"]
-
-        ["X","X","X","X"]
-        ["X","X","X","X"]
-        ["X","X","X","X"]
-        ["X","O","X","X"]
         """
         m, n = len(board), len(board[0])
 
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        visited = set()
 
-        def dfs(r, c, letter):
-            board[r][c] = letter
-
-            visited.add((r, c))
+        def dfs(r, c):
+            board[r][c] = "E"
 
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if (0 <= nr < m and 0 <= nc < n) and (nr, nc) not in visited and board[nr][nc] != "X":
-                    dfs(nr, nc, letter)
+                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == "O":
+                    dfs(nr, nc)
 
-        
         for r in range(m):
             for c in range(n):
                 if (r == 0 or r == m - 1 or c == 0 or c == n - 1) and board[r][c] == "O":
-                    dfs(r, c, "E")
-
-
-        for i in range(m):
-            print(board[i])
+                    dfs(r, c)
 
         for r in range(m):
             for c in range(n):
-                if board[r][c] == "O":
-                    dfs(r, c, "X")
-                elif board[r][c] == "E":
-                    dfs(r, c, "O")
+                board[r][c] = "O" if board[r][c] == "E" else "X"
 
                     
