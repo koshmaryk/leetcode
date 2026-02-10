42aeed444cb1e3686759e5dd8d72fdebada6351e
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-10 22:24:27 +0200

    Time: 0 ms (100%), Space: 21.3 MB (33.33%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -8,21 +8,19 @@ class Solution:
     '''
     def countBattleships(self, board: List[List[str]]) -> int:
         m, n = len(board), len(board[0])
-        visited = set()
-
 
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         def dfs(r, c):
-            visited.add((r, c))
+            board[r][c] = "."
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == "X" and (nr, nc) not in visited:
+                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == "X":
                     dfs(nr, nc)
 
         ans = 0
         for r in range(m):
             for c in range(n):
-                if (r, c) not in visited and board[r][c] == "X":
+                if board[r][c] == "X":
                     dfs(r, c)
                     ans += 1
         return ans
