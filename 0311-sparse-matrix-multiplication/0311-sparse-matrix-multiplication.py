4744a5b8bf38ff2f1f021b2ea34eb6cdd514eeff
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-11 14:05:27 +0200

    Time: 0 ms (100%), Space: 19.5 MB (70.77%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -15,6 +15,10 @@ class Solution:
         mat = [[0] * N for _ in range(M)]
         for i in range(M):
             for k in range(K):
+                if mat1[i][k] == 0:
+                    continue
                 for j in range(N):
+                    if mat2[k][j] == 0:
+                        continue
                     mat[i][j] += mat1[i][k] * mat2[k][j]
         return mat
