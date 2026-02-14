4a6dfa44d8b696d394fc4c313e25145f7444a181
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-14 23:09:57 +0200

    Time: 645 ms (42.16%), Space: 22.3 MB (35.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,29 +1,50 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # [-1,0,1,2,-1,-4]
-        nums.sort()
-        #   |  |  |
-        # [-4,-1,-1,0,1,2]
-        n = len(nums)
-        triplets = []
-        for i in range(0, n):
-            if i > 0 and nums[i - 1] == nums[i]:
-                continue
-            l, r = i + 1, n - 1
-            while l < r:
-                if nums[i] + nums[l] + nums[r] == 0:
-                    while l < r and nums[l] == nums[l + 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r  - 1]:
-                        r -= 1
-                    
-                    triplets.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                elif nums[i] + nums[l] + nums[r] > 0:
-                    r -= 1
-                else:
-                    l += 1
+    '''
+    0,2,3
+    -2,0,5,-3,7
 
+    -3,-2,0,5,7
+
+    -3,-2,-2,0,5,5
+
+    -3,-2,5
+    [0,1,5], [0,2,4]
+
+    i=0
+    nums[i] = -3
+
+    l=i+1; r=n-1
+
+    1) nums[i] + nums[j] + nums[k] == 0
+    2) nums[i] + nums[j] + nums[k] > 0 r -= 1
+    2) nums[i] + nums[j] + nums[k] < 0 l += 1
+
+
+
+    '''
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+
+        nums.sort()
+        triplets = []
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j, k = i + 1, n - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                else:
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+
+                    triplets.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
         return triplets
-        
