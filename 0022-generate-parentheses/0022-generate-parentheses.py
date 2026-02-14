59d7caa61711ae2745093afa4c0a31a8e9d7974d
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-14 23:45:08 +0200

    Time: 0 ms (100%), Space: 19.5 MB (27.31%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,25 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def valid(s):
-            cnt = 0
-            for c in s:
-                if c == '(':
-                    cnt += 1
-                if c == ')':
-                    cnt -= 1
-                if cnt < 0:
-                    return False
-            return cnt == 0
-
         output = []
-        def gen(prefix):
-            if len(prefix) == 2*n:
-                if valid(prefix):
-                    output.append(prefix)
+        stack = []
+
+        def gen(opened, closed):
+            if opened == n and closed == n:
+                output.append("".join(stack))
                 return
 
-            gen(prefix + ')')
-            gen(prefix + '(')
-        
-        gen("")
+            if opened < n:
+                stack.append("(")
+                gen(opened + 1, closed)
+                stack.pop()
+
+            if closed < opened:
+                stack.append(")")
+                gen(opened, closed + 1)
+                stack.pop()
+
+        gen(0, 0)
         return output
