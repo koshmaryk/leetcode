cadb1e835c30de6df185ee67e9969eaec6df4741
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-14 23:23:46 +0200

    Time: 0 ms (100%), Space: 19.5 MB (23.69%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -4,22 +4,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
+    3
+  2   1
+5   0
+
+325 + 320 + 31 = xxx
+
+3 * 10 + 2 = 32
+
+32 * 10 + 5 = 325
+
+node 2 = 325 + 320
+node 3 = 645 + 31
+
+'''
 class Solution:
-
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total_sum = 0
-
-        def dfs(node, number): # 4 | 49 & 40 | - & 495,491
-            nonlocal total_sum
+        total = 0
+        
+        def dfs(node, number):
+            nonlocal total
             if not node.left and not node.right:
-                total_sum += number # 40 + 495 + 491
+                total += number
                 return
 
             if node.left:
-                dfs(node.left, number * 10 + node.left.val) # 4 * 10 + 9 | 49 * 10 + 5
+                dfs(node.left, number * 10 + node.left.val)
             if node.right:
-                dfs(node.right, number * 10 + node.right.val) # 4 * 10 + 0 | 49 * 10 + 1
+                dfs(node.right, number * 10 + node.right.val)
 
         dfs(root, root.val)
-        return total_sum
-        
+        return total
