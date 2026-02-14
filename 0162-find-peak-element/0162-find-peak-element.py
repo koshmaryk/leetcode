f068684d9881982a64840adf45995d9a12404e4c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-14 21:10:45 +0200

    Time: 0 ms (100%), Space: 19.4 MB (55.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,16 +1,15 @@
 class Solution:
     '''
-        1,2,3,1
+
+    5,2,3,4 => 0
+    2,3,4,5 => 4
+    2,3,1,0 => 1
 
 
     '''
     def findPeakElement(self, nums: List[int]) -> int:
         n = len(nums)
-        bad, good = -1, n - 1 # -1,3 | 1,3 | 1,2
-        while good - bad > 1: # 4 | 2 | 1
-            mid = (bad + good) // 2 # 1 | 2
-            if nums[mid] > nums[mid + 1]: # 2 > 3 | 3 > 1
-                good = mid
-            else:
-                bad = mid
-        return good # 2
+        for i in range(n - 1):
+            if nums[i] > nums[i + 1]:
+                return i
+        return n - 1
