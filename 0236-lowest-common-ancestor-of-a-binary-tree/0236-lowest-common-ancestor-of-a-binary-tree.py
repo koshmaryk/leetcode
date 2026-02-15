a954d5ca8d2a4694ef6c457ac3381c2666aae774
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-15 13:36:05 +0200

    Time: 57 ms (60.93%), Space: 23.7 MB (67.88%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -4,21 +4,47 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 
 class Solution:
     '''
     case 1: p == q
     case 2: p is LCA of q or vice versa
     case 3: x is LCA of p and q
+
+            3
+        5       1
+    6       2       7
+8       4
+
+p=4
+q=2
+
+6->4; 6->8; 5->6; 5->2; 3->5; 1->7; 3->1
+
+set = [4, 6, 5, 3]
+
+
+
     '''
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or p == root or q == root:
-            return root
+        parent = {root: None}
+        queue = deque([root])
+        while p not in parent or q not in parent:
+            node = queue.popleft()
+            if node.left:
+                parent[node.left] = node
+                queue.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                queue.append(node.right)
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
 
-        if left and right:
-            return root
-
-        return left if left else right
+        while q not in ancestors:
+            q = parent[q]
+        return q
+        
