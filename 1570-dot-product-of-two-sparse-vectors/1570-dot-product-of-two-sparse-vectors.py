bb43c786e3c23adf6a233d0714622061e3380c78
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-15 14:57:59 +0200

    Time: 1206 ms (36.12%), Space: 24 MB (12.09%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
@@ -1,22 +1,38 @@
+'''
+0,0,5,5,5,0,1,3,3
+
+'''
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.nonzero = []
-        for idx, num in enumerate(nums):
-            if num != 0:
-                self.nonzero.append((idx, num))
+        self.ranges = []
+        i = 0
+        while i < len(nums):
+            if nums[i] != 0:
+                start = i
+                val = nums[i]
+                while i < len(nums) and nums[i] == val:
+                    i += 1
+                self.ranges.append((start, i - 1, val))
+            else:
+                i += 1
+        
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
         product = 0
         p, q = 0, 0
-        while p < len(self.nonzero) and q < len(vec.nonzero):
-            if self.nonzero[p][0] < vec.nonzero[q][0]:
+        while p < len(self.ranges) and q < len(vec.ranges):
+            s1, e1, v1 = self.ranges[p]
+            s2, e2, v2 = vec.ranges[q]
+
+            start = max(s1, s2)
+            end = min(e1, e2)
+            if start <= end:
+                product += v1 * v2 * (end - start + 1)
+            
+            if e1 < e2:
                 p += 1
-            elif self.nonzero[p][0] > vec.nonzero[q][0]:
-                q += 1
             else:
-                product += self.nonzero[p][1] * vec.nonzero[q][1]
-                p += 1
                 q += 1
         return product
         
