947d209bdfa3bd6f8d98855e750a6156bcce8242
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-24 23:08:28 +0200

    Time: 55 ms (31.6%), Space: 31.9 MB (23.57%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -3,8 +3,12 @@ class Solution:
     4,3,2,1
     0,1,2,3
 
+    0,1,2,3
+
     4,2,3,1
     0,2,3
+
+    4,3,1
 
     1,2,3,4
     3
@@ -13,12 +17,16 @@ class Solution:
     '''
     def findBuildings(self, heights: List[int]) -> List[int]:
         n = len(heights)
+        answer = [] # 1,3,4
 
-        stack = [] # 0,2,3
-        for i in range(n):
-            while stack and heights[stack[-1]] <= heights[i]:
+        stack = [] # 4
+        for i in range(n - 1, -1, -1):
+            while stack and heights[stack[-1]] < heights[i]:
                 stack.pop()
+
+            if not stack:
+                answer.append(i)
 
             stack.append(i)
 
-        return stack
+        return answer[::-1]
