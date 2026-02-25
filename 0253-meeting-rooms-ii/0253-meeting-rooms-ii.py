a61db7d7945b997194b9c951b1a3d85005370642
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-25 23:50:46 +0200

    Time: 3 ms (84.55%), Space: 21.5 MB (64.73%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,22 +1,13 @@
+import heapq
+
 class Solution:
-    '''
-    [[0,30],[5,10],[15,20]]
-
-    [(0, 1), (5, 1), (10, -1), (15, 1), (20, -1), (30, -1)]
-
-    2  1 2 1 0
-    1+1-1+1-1-1
-
-    '''
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        events = []
+        intervals.sort(key=lambda x: x[0])
+        pq = []
         for s,e in intervals:
-            events.append((s, 1))
-            events.append((e, -1))
+            if pq and pq[0] <= s:
+                heapq.heappop(pq)
 
-        events.sort()
-        ans = curr = 0
-        for _, weight in events:
-            curr += weight
-            ans = max(ans, curr)
-        return ans
+            heapq.heappush(pq, e)
+
+        return len(pq)
