e5b06e412007de6868594c80304be9f31655dcc0
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-25 23:46:19 +0200

    Time: 3 ms (84.55%), Space: 22.2 MB (10.13%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,4 +1,13 @@
 class Solution:
+    '''
+    [[0,30],[5,10],[15,20]]
+
+    [(0, 1), (5, 1), (10, -1), (15, 1), (20, -1), (30, -1)]
+
+    2  1 2 1 0
+    1+1-1+1-1-1
+
+    '''
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         events = []
         for s,e in intervals:
