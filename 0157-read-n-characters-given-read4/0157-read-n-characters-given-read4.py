b68e135de3349b05e2d7262c4e386f9af719fa0d
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-01 01:19:43 +0200

    Time: 43 ms (55.98%), Space: 19.2 MB (80.48%) - LeetHub

---
+++ 0157-read-n-characters-given-read4/0157-read-n-characters-given-read4.py
@@ -0,0 +1,47 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+""" 
+
+while loop:
+1) we've read n chars
+2) we've read < 4 chars
+
+
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        buffered_chars = 0
+        read_chars = 4
+        buf4 = [''] * 4
+
+        while buffered_chars < n and read_chars == 4:
+            read_chars = read4(buf4)
+
+            for i in range(read_chars):
+                buf[buffered_chars] = buf4[i]
+                buffered_chars += 1
+                if buffered_chars == n:
+                    return buffered_chars
+
+        return buffered_chars
+
+        
