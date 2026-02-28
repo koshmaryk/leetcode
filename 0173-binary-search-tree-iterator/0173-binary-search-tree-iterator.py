7360f92a9cee63ed8c1d07a42992839f7d038364
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-02-28 12:52:25 +0200

    Time: 16 ms (7.02%), Space: 25.6 MB (49.74%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -10,42 +10,104 @@
     1   3      5    7
 
 
-        4
-    2
-1      3
+stack = 4 6 2
+
+p = 2
 
 
-[]
 
-p = 
+LRC
 
-7
 '''
+# class BSTIterator:
+
+#     def __init__(self, root: Optional[TreeNode]):
+#         self.stack = 
+
+#     def next(self) -> int:
+#         if not self.hasNext():
+#             return -1
+
+
+        
+#     def hasNext(self) -> bool:
+#         if self.stack:
+#             return True
+#         return False
+
+# class BSTIterator:
+
+#     def __init__(self, root: Optional[TreeNode]):
+#         self.p = root
+#         self.stack = []
+        
+
+#     def next(self) -> int:
+#         if not self.hasNext():
+#             return -1
+
+#         while self.p:
+#             self.stack.append(self.p)
+#             self.p = self.p.left
+
+#         self.p = self.stack.pop()
+#         val = self.p.val
+#         self.p = self.p.right
+#         return val
+        
+
+#     def hasNext(self) -> bool:
+#         if self.stack or self.p:
+#             return True
+#         return False
+
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.p = root
-        self.stack = []
-        
+        self.stack = [(root, False)] if root else []
 
     def next(self) -> int:
         if not self.hasNext():
             return -1
 
-        while self.p:
-            self.stack.append(self.p)
-            self.p = self.p.left
-
-        self.p = self.stack.pop()
-        val = self.p.val
-        self.p = self.p.right
-        return val
+        while self.stack:
+            node, visited = self.stack.pop()
+            if visited:
+                return node.val
+            if node.right:
+                self.stack.append((node.right, False))
+            self.stack.append((node, True))
+            if node.left:
+                self.stack.append((node.left, False))
         
-
     def hasNext(self) -> bool:
-        if self.stack or self.p:
+        if self.stack:
             return True
         return False
+
+# class BSTIterator:
+
+#     def __init__(self, root: Optional[TreeNode]):
+#         self.stack = [root]
+
+#     def next(self) -> int:
+#         if not self.hasNext():
+#             return -1
+
+#         ptr = self.stack.pop()
+#         val = ptr.val
+
+#         if ptr.right:
+#             self.stack.append(ptr.right)
+#         if ptr.left:
+#             self.stack.append(ptr.left)
+
+#         return val
+        
+#     def hasNext(self) -> bool:
+#         if self.stack:
+#             return True
+#         return False
         
 
 
