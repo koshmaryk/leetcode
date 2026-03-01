39e25f29ac005f6772b0c1f2b8613dce1a69f1c6
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-01 15:43:40 +0200

    Time: 2 ms (74.23%), Space: 20 MB (82.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,45 +1,19 @@
 class Solution:
-    '''
-        rows [0,0,0,0]
-        cols [0,0,0]
-
-        [1,1,2,0],
-        [1,4,0,2],
-        [1,3,1,5]
-
-        [0,1,2,0],
-        [3,4,5,2],
-        [1,3,1,5]
-
-        [0,1,0,0],
-        [0,4,0,2],
-        [1,3,1,5]
-
-
-        [0,0,0,0],
-        [0,4,5,0],
-        [0,3,1,0]
-
-    '''
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         m, n = len(matrix), len(matrix[0])
-        zero_col = False
-
+        zero_rows  = set()
+        zero_cols  = set()
         for r in range(m):
-            if matrix[r][0] == 0:
-                zero_col = True
-            for c in range(1, n):
+            for c in range(n):
                 if matrix[r][c] == 0:
-                    matrix[r][0] = 0
-                    matrix[0][c] = 0
+                    zero_rows.add(r)
+                    zero_cols.add(c)
         
-        for r in range(m - 1, -1, -1):
-            for c in range(n - 1, 0, -1):
-                if matrix[r][0] == 0 or matrix[0][c] == 0:
+        for r in range(m):
+            for c in range(n):
+                if r in zero_rows or c in zero_cols:
                     matrix[r][c] = 0
-
-            if zero_col:
-                matrix[r][0] = 0
+        
