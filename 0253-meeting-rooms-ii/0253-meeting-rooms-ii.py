0b435005f292ec6c570e2940e74c782d0c8e98de
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-03 23:56:44 +0200

    Time: 3 ms (84.18%), Space: 21.4 MB (86.96%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,13 +1,25 @@
+"""
+
+0,10; 10,20; 20,30 -> 1
+0,10; 5,20; 0,30 -> 3
+0,10; 5,20; 10,30 -> 2
+
+1. sort by start
+2. pop if earliest end less then curr start
+3. add to
+
+
+
+"""
 import heapq
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort()
+
         pq = []
         for s,e in intervals:
             if pq and pq[0] <= s:
                 heapq.heappop(pq)
-
             heapq.heappush(pq, e)
-
         return len(pq)
