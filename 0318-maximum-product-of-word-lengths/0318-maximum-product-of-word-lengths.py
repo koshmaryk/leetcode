20cdb1b359aaa59922210d42dee09685d7173aba
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-03 16:30:07 +0200

    Time: 829 ms (35.05%), Space: 22.5 MB (44.64%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,16 +1,12 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        def no_common_letters(word1, word2):
-            for c in word1:
-                if c in word2:
-                    return False
-            return True
+        char_sets = [set(word) for word in words]
 
         n = len(words)
         max_product = 0
         for i in range(n):
             for j in range(i + 1, n):
-                if no_common_letters(words[i], words[j]):
+                if not (char_sets[i] & char_sets[j]):
                     max_product = max(max_product, len(words[i]) * len(words[j]))
         return max_product
         
