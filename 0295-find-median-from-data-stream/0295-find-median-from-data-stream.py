20010a98845396d78fb06b46aead35c196672baa
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-04 11:11:00 +0200

    Time: 147 ms (86.41%), Space: 42.2 MB (38.03%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -5,7 +5,6 @@ class MedianFinder:
     def __init__(self):
         self.low = []
         self.high = []
-        
 
     def addNum(self, num: int) -> None:
         if not self.low or -self.low[0] > num:
@@ -15,29 +14,19 @@ class MedianFinder:
 
         if len(self.low) > len(self.high) + 1:
             heapq.heappush(self.high, -heapq.heappop(self.low))
-        elif len(self.high) > len(self.low) + 1:
+        elif len(self.low) + 1 < len(self.high):
             heapq.heappush(self.low, -heapq.heappop(self.high))
 
-
-    def findMedian(self) -> flowat:
+    def findMedian(self) -> float:
         if len(self.low) > len(self.high):
             return -self.low[0]
         elif len(self.low) < len(self.high):
             return self.high[0]
         else:
             return (-self.low[0] + self.high[0]) / 2.0
-        
-        
 
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
-#
-# 1, 4, 6, 7
-# low/MaxHeap [3, 2, 1]
-# high/MinHeap [4, 6, 7]
-
-# low/MaxHeap [3, 2, 1]
-# high/MinHeap [4, 6, 7]
