da28c1c46a720c2f69d1ae379dc6a97dc3a16413
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-29 20:48:29 +0300

    Time: 3 ms (56.48%), Space: 19.6 MB (88.1%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -10,13 +10,12 @@ class UnionFind:
             a = self.parent[a]
         return self.parent[a]
 
-
     def union(self, a, b):
         parent_a, parent_b = self.find(a), self.find(b)
         if parent_a == parent_b:
             return False
 
-        if self.rank[parent_a] > self.rank[parent_b]:
+        if self.rank[parent_a] >= self.rank[parent_b]:
             self.parent[parent_b] = parent_a
             self.rank[parent_a] += self.rank[parent_b]
         else:
@@ -24,12 +23,11 @@ class UnionFind:
             self.rank[parent_b] += self.rank[parent_a]
         return True
 
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
         uf = UnionFind(n + 1)
         for a,b in edges:
             if not uf.union(a, b):
-                return [a,b]
+                return [a, b]
         return [-1, -1]
