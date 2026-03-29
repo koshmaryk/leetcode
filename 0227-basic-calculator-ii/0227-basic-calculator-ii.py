a8a8bcc90ad472ec5a19237e7151183deb981d9d
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-29 18:18:38 +0300

    Time: 59 ms (51.27%), Space: 22.4 MB (51.64%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,31 +1,39 @@
 """
 
-3+5/2
++3+5/2+10
+
+op = +
+number = 10
+
+iter = 0
+
+stack = 15
 
 """
 class Solution:
     def calculate(self, s: str) -> int:
         stack = []
-        curr_number = 0
-        op = '+'
+        op = "+"
+        number = 0
         for i, c in enumerate(s):
             if c.isdigit():
-                curr_number = curr_number * 10 + int(c)
+                number = number * 10 + int(c)
 
-            if c in '+-*/' or i == len(s) - 1:
-                if op == '+':
-                    stack.append(curr_number)
-                elif op == '-':
-                    stack.append(-curr_number)
-                elif op == '*':
-                    stack.append(stack.pop() * curr_number)
+            if c in "+-*/" or i == len(s) - 1:
+                if op == "+":
+                    stack.append(number)
+                elif op == "-":
+                    stack.append(-number)
+                elif op == "*":
+                    stack.append(stack.pop() * number)
                 else:
-                    stack.append(int(stack.pop() / curr_number))
+                    stack.append(int(stack.pop() / number))
+
                 op = c
-                curr_number = 0
-        
+                number = 0
+
         ans = 0
         while stack:
             ans += stack.pop()
-        return ans 
+        return ans
         
