0dd049ca3f86138e1b60e0a390d2c079ad342f23
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-29 18:39:58 +0300

    Time: 67 ms (15.03%), Space: 20.8 MB (47.7%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -27,21 +27,21 @@ class Solution:
         for i in range(n):
             if s[i] == "(":
                 cnt += 1
-            if s[i] == ")":
+            elif s[i] == ")":
                 if cnt == 0:
                     invalid.add(i)
-                    continue
-                cnt -= 1
+                else:
+                    cnt -= 1
 
         cnt = 0
         for i in range(n - 1, -1, -1):
             if s[i] == ")":
                 cnt += 1
-            if s[i] == "(":
+            elif s[i] == "(":
                 if cnt == 0:
                     invalid.add(i)
-                    continue
-                cnt -= 1
+                else:
+                    cnt -= 1
 
         sb = []
         for i in range(n):
