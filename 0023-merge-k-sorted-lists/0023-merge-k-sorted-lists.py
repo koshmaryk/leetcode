f6d31273469f8185e1c84beebe12a12c11b75468
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-30 14:03:21 +0300

    Time: 11 ms (56.73%), Space: 22.8 MB (64.45%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,38 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 
-"""
-
-
-[1,4,5],[1,3,4],[2,6]
-
-1->1->3->4->4->5
-
-1->1->2->3->4->4->5->6
-
-"""
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def merge(l1, l2):
-            sentinel = curr = ListNode()
-            while l1 and l2:
-                if l1.val < l2.val:
-                    curr.next = l1
-                    l1 = l1.next
-                else:
-                    curr.next = l2
-                    l2 = l2.next
-                curr = curr.next
+        pq = []
+        for i, l in enumerate(lists):
+            if l:
+                pq.append((l.val, i, l))
 
-            curr.next = l1 if l1 else l2
+        heapq.heapify(pq)
 
-            return sentinel.next
+        sentinel = curr = ListNode()
+        while pq:
+            _, i, l = heapq.heappop(pq)
+            curr.next = l
+            curr = curr.next
 
-        if not lists:
-            return None
-
-        n = len(lists)
-        for i in range(1, n):
-            lists[i] = merge(lists[i - 1], lists[i])
-        return lists[-1]
+            if l.next:
+                heapq.heappush(pq, (l.next.val, i, l.next))
+        return sentinel.next
