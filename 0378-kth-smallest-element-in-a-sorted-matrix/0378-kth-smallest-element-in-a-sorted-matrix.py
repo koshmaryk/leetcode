7192d0222a4b90c32ffb53336f3fb4d074011699
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-30 12:53:58 +0300

    Time: 7 ms (62.35%), Space: 22.8 MB (95.01%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -10,23 +10,11 @@ class Solution:
 
     '''
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        def countLessOrEqual(num): # 13
-            cnt = 0
-            r, c = 0, n - 1 # 0,2 | 1,2 | 2,2 | 2,1 | 3,1
-            while r < n and c >= 0:
-                if matrix[r][c] <= num:
-                    cnt += c + 1 # 0+3+3+2
-                    r += 1
-                else:
-                    c -= 1
-            return cnt
-
         n = len(matrix)
-        bad, good = matrix[0][0] - 1, matrix[n - 1][n - 1] + 1 # 0,16 | 8,16 | 12,16 | 12,14 | 12,13
-        while good - bad > 1:
-            guess = (bad + good) // 2 # 8 | 12 | 14 | 13
-            if countLessOrEqual(guess) >= k:
-                good = guess
-            else:
-                bad = guess
-        return good
+        flat = [0] * (n * n)
+        for r in range(n):
+            for c in range(n):
+                flat[r * n + c] = matrix[r][c]
+
+        flat.sort()
+        return flat[k - 1]
