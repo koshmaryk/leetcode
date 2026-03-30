ed59b90a649a0f56dfacde376ef9e7771018da1a
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-03-30 13:39:41 +0300

    Time: 951 ms (11.15%), Space: 22.6 MB (86.5%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,11 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+"""
+
+
+[1,4,5],[1,3,4],[2,6]
+
+1->1->3->4->4->5
+
+1->1->2->3->4->4->5->6
+
+"""
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         def merge(l1, l2):
-            sentinel = ListNode()
-            curr = sentinel
+            sentinel = curr = ListNode()
             while l1 and l2:
                 if l1.val < l2.val:
                     curr.next = l1
@@ -24,7 +34,7 @@ class Solution:
         if not lists:
             return None
 
-        for i in range(1, len(lists)):
+        n = len(lists)
+        for i in range(1, n):
             lists[i] = merge(lists[i - 1], lists[i])
-
         return lists[-1]
