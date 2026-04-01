3b74b35db760a2715ed6e6f803b2289e812fa8e7
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-04-01 14:35:52 +0300

    Time: 43 ms (23.01%), Space: 22.4 MB (19.13%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,26 +1,42 @@
+"""
+
+k = 3
+
+expected = 3
+
+0,1,3,6,4,6,11
+
+1,2,3,-2,2,5
+
+3
+
+{0:1, 1:1, 3:1, 6:1}
+
+
+
+
+p[j] - p[i] = k
+p[i] = p[j] - k
+
+actual = 1
+
+"""
+from collections import defaultdict
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        #
-        #   1,2,3 k = 3
-        # 0 1 3 6
-        #
-        #   1 -1 1 -1 0 k = 0
-        # 0 1  0 1  0 0
-        #
-        #   1 2 -1 -1 k = 1
-        # 0 1 3  2  1
-        # 
-        # k= 5; p = 50, -3 2 1 4 1 0
-
         n = len(nums)
-        ans = 0 # 2
+        
+        count = defaultdict(int)
+        count[0] = 1
 
-        p = 0 # 3
-        count = {0: 1} # 1:1; 3:1; 6:1
+        ans = 0
+        p = 0
         for i in range(n):
-            p += nums[i] # 1; 3; 6
+            p += nums[i]
             if p - k in count:
                 ans += count[p - k]
-            count[p] = count.get(p, 0) + 1
 
+            count[p] += 1
         return ans
+        
