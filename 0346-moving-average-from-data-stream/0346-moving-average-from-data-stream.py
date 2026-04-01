7c69a9866d3e9b03a843955c61ac2b1c059dc649
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-04-01 13:33:43 +0300

    Time: 4 ms (72.16%), Space: 22.2 MB (55.93%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class MovingAverage:
 
     '''
@@ -9,21 +11,15 @@ class MovingAverage:
 
     def __init__(self, size: int):
         self.size = size
-        self.queue = [0] * self.size
-        self.head = 0
-        self.tail = (self.head + 1) % self.size
+        self.window = deque()
         self.total = 0
-        self.count = 0
         
 
     def next(self, val: int) -> float:
-        self.total -= self.queue[self.tail]
-        self.total += val
-        self.head = (self.head + 1) % self.size
-        self.tail = (self.tail + 1) % self.size
-        self.queue[self.head] = val
-        self.count += 1
-        return self.total / min(self.size, self.count)
+        self.window.append(val)
+        tail = self.window.popleft() if len(self.window) > self.size else 0
+        self.total = self.total - tail + val
+        return self.total / len(self.window)
 
         
 
