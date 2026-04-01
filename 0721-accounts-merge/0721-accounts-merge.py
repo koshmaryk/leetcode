c594174e3556e6e464410304eb5a8118c2ad3ca9
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-04-01 18:02:48 +0300

    Time: 35 ms (44.24%), Space: 25.3 MB (19.37%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -12,48 +12,32 @@ bob@mail.com -> 3
 """
 from collections import defaultdict
 
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [1] * n
-
-    def find(self, x):
-        while self.parent[x] != x:
-            x = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        root_x, root_y = self.find(x), self.find(y)
-        if root_x == root_y:
-            return
-
-        if self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x
-            self.rank[root_x] += self.rank[root_y]
-        else:
-            self.parent[root_x] = root_y
-            self.rank[root_y] += self.rank[root_x]
-
-
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        n = len(accounts)
-        uf = UnionFind(n)
+        email_to_ids = defaultdict(list)
+        for i, account in enumerate(accounts):
+            for email in account[1:]:
+                email_to_ids[email].append(i)
 
-        email_to_id = {}
-        for i in range(n):
+        visited = [False] * len(accounts)
+
+        def dfs(i, emails):
+            if visited[i]:
+                return
+
+            visited[i] = True
+
             for email in accounts[i][1:]:
-                if email in email_to_id:
-                    uf.union(i, email_to_id[email])
-                else:
-                    email_to_id[email] = i
+                emails.add(email)
+                for neighbor in email_to_ids[email]:
+                    dfs(neighbor, emails)
 
-        id_to_emails = defaultdict(list)
-        for email, id in email_to_id.items():
-            root_id = uf.find(id)
-            id_to_emails[root_id].append(email)
 
         output = []
-        for root_id, emails in id_to_emails.items():
-            output.append([accounts[root_id][0]] + sorted(emails))
+        for i, account in enumerate(accounts):
+            if not visited[i]:
+                emails = set()
+                dfs(i, emails)
+                output.append([account[0]] + sorted(emails))
         return output
+        
