95e8ac92d1416f1769bd1fd34e47203e7a8a5aa5
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-04-26 22:34:57 +0300

    Time: 590 ms (7.81%), Space: 53.5 MB (18.22%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,29 +1,29 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         graph = defaultdict(list)
-        indegree = [0] * n
         for f,t in edges:
-            graph[f].append(t)
-            indegree[t] += 1
+            graph[t].append(f)
 
-        queue = deque([node for node in range(n) if indegree[node] == 0])
+        def dfs(u):
+            visited.add(u)
 
-        ancestors = [set() for _ in range(n)]
+            for v in graph[u]:
+                if v not in visited:
+                    dfs(v)
 
-        ts = []
-        while queue:
-            node = queue.popleft()
-            ts.append(node)
+        answer = []
+        for u in range(n):
+            visited = set()
+            dfs(u)
 
-            for next_node in graph[node]:
-                ancestors[next_node].add(node)
-                ancestors[next_node].update(ancestors[node])
+            ancestors = []
+            for node in range(n):
+                if node in visited and node != u:
+                    ancestors.append(node)
+                    
+            answer.append(ancestors)
+        return answer
 
-                indegree[next_node] -= 1
-                if indegree[next_node] == 0:
-                    queue.append(next_node)
-
-        return [sorted(ancestors[node]) for node in range(n)]
             
