ec8304bcd2439bca1ce8754fe0bb0622111e168e
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-04-28 15:04:29 +0300

    Time: 915 ms (60.87%), Space: 42.4 MB (76.09%) - LeetHub

--- 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.py
+++ 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.py
@@ -1,19 +1,19 @@
 class Solution:
     def waysToPartition(self, nums: List[int], k: int) -> int:
-        N = len(nums)
+        n = len(nums)
         diffs = []
-        sl, sr = 0, sum(nums)
-        for i in range(N - 1):
-            sl += nums[i]
-            sr -= nums[i]
-            diffs.append(sl - sr)
+        lsum, rsum = 0, sum(nums)
+        for i in range(n - 1):
+            lsum += nums[i]
+            rsum -= nums[i]
+            diffs.append(lsum - rsum)
         
         lcounter, rcounter = Counter(), Counter(diffs)
         ans = lcounter[0] + rcounter[0]
-        for i in range(N):
+        for i in range(n):
             d = k - nums[i]
             ans = max(ans, lcounter[d] + rcounter[-d])
-            if i < N - 1:
+            if i < n - 1:
                 lcounter[diffs[i]] += 1
                 rcounter[diffs[i]] -= 1
         return ans
