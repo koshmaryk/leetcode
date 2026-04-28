b10489e601e4d99b50591c6bd90073ec3da96a19
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-04-28 15:41:26 +0300

    Time: 930 ms (60.87%), Space: 42.9 MB (69.57%) - LeetHub

--- 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.py
+++ 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.py
@@ -1,3 +1,16 @@
+"""
+ans = 0
+
+k = 3
+
+nums 2,-1,2
+diffs 1,-1,inf
+
+
+
+"""
+from collections import Counter
+
 class Solution:
     def waysToPartition(self, nums: List[int], k: int) -> int:
         n = len(nums)
@@ -7,12 +20,18 @@ class Solution:
             lsum += nums[i]
             rsum -= nums[i]
             diffs.append(lsum - rsum)
-        
+
         lcounter, rcounter = Counter(), Counter(diffs)
-        ans = lcounter[0] + rcounter[0]
+        ans = lcounter[0] + rcounter[0] # baseline
         for i in range(n):
+            # changing nums[p] with k shifts every pivot's diff by delta
+            # if i < pivot then (L + delta) - R; (L - R) + delta; old_diff + delta; 
+            # valid if old_diff + delta = 0; old_diff = -delta
+            # if i >= pivot then L - (R + delta); (L - R) - delta; old_diff - delta
+            # valid if old_diff - delta = 0; old_diff = delta
             d = k - nums[i]
             ans = max(ans, lcounter[d] + rcounter[-d])
+
             if i < n - 1:
                 lcounter[diffs[i]] += 1
                 rcounter[diffs[i]] -= 1
