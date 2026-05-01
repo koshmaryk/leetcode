caaa37adac8e3b6c06d5f0b8f00456cfe7732e7b
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-01 21:52:48 +0300

    Time: 4 ms (40.89%), Space: 19.4 MB (52.62%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,37 @@
+"""
+leet
+
+leet + "" (empty base case)
+lee + t
+le + e; l + ee
+l + e; e + e
+
+le + et
+l + eet
+
+
+
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+
+        memo = {}
+
+        def can_break(word):
+            if not word:
+                return True
+
+            if word in memo:
+                return memo[word]
+
+            for i in range(1, len(word) + 1):
+                prefix = word[:i]
+                if prefix in words and can_break(word[i:]):
+                    memo[prefix] = True
+                    return True
+
+            memo[word] = False
+            return memo[word]
+
+        return can_break(s)
