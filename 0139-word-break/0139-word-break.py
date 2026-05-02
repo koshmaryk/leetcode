355fd8c3e9bc36c564288d69839e29aa642efc4c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-02 15:12:50 +0300

    Time: 0 ms (100%), Space: 20.6 MB (5.77%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -11,27 +11,56 @@ l + eet
 
 s[i:i + len(word)] == word and dp[i + len(word)]
 
+leet, code
+
+
+leetcode
 
 """
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
+        
+        root = TrieNode()
+        for word in wordDict:
+            curr = root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.is_word = True
 
         memo = {}
 
         def can_break(i):
-            if i < 0:
+            if i == len(s):
                 return True
 
             if i in memo:
                 return memo[i]
 
-            for word in wordDict:
-                if s[i - len(word) + 1:i + 1] == word and can_break(i - len(word)):
-                    memo[i] = True
-                    return True
+            curr = root
+            for j in range(i, n):
+                c = s[j]
+                curr = curr.children.get(c)
+                if not curr:
+                    break
+
+                if curr.is_word and can_break(j + 1):
+                    memo[j] = True
+                    return True     
+
+            # for word in wordDict:
+            #     if s[i:i + len(word)] == word and can_break(i + len(word)):
+            #         memo[i] = True
+            #         return True
 
             memo[i] = False
             return False
 
-        return can_break(len(s) - 1)
+        return can_break(0)
