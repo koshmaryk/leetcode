54dceb20e926943fd0c51ffe249e00c7f82d5667
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-04 15:35:07 +0300

    Time: 0 ms (100%), Space: 19.4 MB (38.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,31 @@
+"""
+l mid r
+
+which part is sorted, left or right?
+
+0,1,2,3,4,5,6
+
+4,5,6,7,0,1,2
+|     |
+
+4,5,6,0,1,1,2
+
+target = 1
+bad = -1, good = 7
+
+"""
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # 0
-        #
-        # 4,5,6,7,0,1,2
-        # 
-        # 4,5,6,0,1,1,2
-        # 
-        # bad = -1, good = 7
-        # 
         n = len(nums)
         bad, good = -1, n
         while good - bad > 1:
             mid = (bad + good) // 2
             if nums[0] <= nums[mid]:
-                #  left is sorted
                 if nums[0] <= target <= nums[mid]:
                     good = mid
                 else:
                     bad = mid
             else:
-                # right is sorted
                 if nums[mid] < target <= nums[-1]:
                     bad = mid
                 else:
