9351bf47cf9ead9dd44a77e5cf47e27d7d22a66e
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-04 21:58:53 +0300

    Time: 85 ms (13.69%), Space: 20.7 MB (66.69%) - LeetHub

---
+++ 0759-employee-free-time/0759-employee-free-time.py
@@ -0,0 +1,39 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+
+1,2; 5,6
+1,3
+4,10
+
+
+
+
+
+
+
+"""
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        events = []
+        for s in schedule:
+            for interval in s:
+                events.append((interval.start, -1))
+                events.append((interval.end, 1))
+
+        events.sort()
+        
+        output = []
+        prev = float('-inf')
+        balance = 0
+        for timestamp, t in events:
+            if balance == 0 and prev != float('-inf'):
+                output.append(Interval(prev, timestamp))
+
+            balance += t
+            prev = timestamp
+        return output
