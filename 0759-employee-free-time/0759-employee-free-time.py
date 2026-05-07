9c049aad0b26bfcef9505d7989802ede78bea994
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-07 12:14:38 +0300

    Time: 67 ms (83.6%), Space: 20.7 MB (66.46%) - LeetHub

--- 0759-employee-free-time/0759-employee-free-time.py
+++ 0759-employee-free-time/0759-employee-free-time.py
@@ -11,25 +11,30 @@ class Interval:
 
 
 """
+import heapq
 
 class Solution:
     def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
-        events = []
-        for s in schedule:
-            for interval in s:
-                events.append((interval.start, 1))
-                events.append((interval.end, -1))
-
-        events.sort(key=lambda e: (e[0], -e[1]))
+        pq = []
+        for idx, employee in enumerate(schedule):
+            interval = employee[0]
+            pq.append((interval.start, interval.end, idx, 0))
+        heapq.heapify(pq)
 
         output = []
 
-        prev = float('-inf')
-        balance = 0
-        for timestamp, event_type in events:
-            if balance == 0 and prev != float('-inf'):
-                output.append(Interval(prev, timestamp))
+        prev_end = pq[0][1]
+        while pq:
+            start, end, employee_idx, interval_idx = heapq.heappop(pq)
 
-            balance += event_type
-            prev = timestamp
+            if prev_end < start:
+                output.append(Interval(prev_end, start))
+
+            prev_end = max(prev_end, end)
+
+            if interval_idx + 1 < len(schedule[employee_idx]):
+                next_interval = schedule[employee_idx][interval_idx + 1] 
+                heapq.heappush(pq, (next_interval.start, next_interval.end, employee_idx, interval_idx + 1))
         return output
+
+
