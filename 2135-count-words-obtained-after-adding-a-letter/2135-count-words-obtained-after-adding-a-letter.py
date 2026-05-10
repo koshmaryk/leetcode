5737c2f70aefd965516146540ab3a9e9c643a589
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-10 11:33:06 +0300

    Time: 414 ms (39.36%), Space: 34.6 MB (69.15%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -1,19 +1,28 @@
 """
 
-TC O((N + M) * L * L log L)
+TC O((N + M) * L)
 SC O(M * L)
 
 
 """
 class Solution:
     def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
-        words = set(["".join(sorted(start)) for start in startWords])
+        def mask(word):
+            mask = 0
+            for c in word:
+                mask |= (1 << (ord(c) - ord('a')))
+            return mask
+
+        words = set()
+        for start in startWords:
+            s = mask(start)
+            words.add(s)
 
         ans = 0
         for target in targetWords:
-            t = "".join(sorted(target))
-            for i in range(len(t)):
-                if t[:i] + t[i + 1:] in words:
+            t = mask(target)
+            for c in target:
+                if (t ^ (1 << (ord(c) - ord('a')))) in words:
                     ans += 1
                     break
         return ans
