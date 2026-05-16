e73d78c4cc70fbee48ed0c858930acb6d967dd31
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-16 13:30:37 +0300

    Time: 7 ms (81.11%), Space: 22.3 MB (65.57%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,3 +1,5 @@
+import bisect
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         n = len(searchWord)
@@ -6,12 +8,12 @@ class Solution:
         outputs = []
         for i in range(1, n + 1):
             prefix = searchWord[:i]
+            good = bisect.bisect_left(products, prefix)
             output = []
-            for product in products:
-                if product.startswith(prefix):
-                    output.append(product)
-
-                if len(output) == 3:
+            for j in range(good, min(good + 3, len(products))):
+                if products[j].startswith(prefix):
+                    output.append(products[j])
+                else:
                     break
 
             outputs.append(output)      
