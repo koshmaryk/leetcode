d42976a5ccdbc5c80649ba30ec54f982cb36efc1
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-17 12:31:41 +0300

    Time: 253 ms (17.45%), Space: 25.8 MB (18.59%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,20 +1,41 @@
-import bisect
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        n = len(searchWord)
         products.sort()
 
-        outputs = []
-        for i in range(1, n + 1):
-            prefix = searchWord[:i]
-            good = bisect.bisect_left(products, prefix)
-            output = []
-            for j in range(good, min(good + 3, len(products))):
-                if products[j].startswith(prefix):
-                    output.append(products[j])
-                else:
-                    break
+        root = TrieNode()
+        for product in products:
+            curr = root
+            for c in product:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.is_end = True
 
-            outputs.append(output)      
-        return outputs
+        output = []
+
+        def dfs(node, prefix, suggestions):
+            if len(suggestions) == 3:
+                return
+            if node.is_end:
+                suggestions.append(prefix)
+            for c in sorted(node.children):
+                dfs(node.children[c], prefix + c, suggestions)
+
+
+        curr = root
+        for i, c in enumerate(searchWord):
+            if curr:
+                curr = curr.children.get(c)
+                
+            suggestions = []
+            if curr:
+                dfs(curr, searchWord[:i+1], suggestions)
+
+            output.append(suggestions)
+        return output
+        
