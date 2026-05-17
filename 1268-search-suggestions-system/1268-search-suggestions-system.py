66bc4c0088b797f78354ce602b426e1a8bbde996
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-17 13:23:28 +0300

    Time: 254 ms (17.45%), Space: 25.7 MB (24.67%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,7 +1,7 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.is_end = False
+        self.is_word = False
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
@@ -12,28 +12,28 @@ class Solution:
                 if c not in curr.children:
                     curr.children[c] = TrieNode()
                 curr = curr.children[c]
-            curr.is_end = True
+            curr.is_word = True
 
-        output = []
 
         def dfs(node, prefix, suggestions):
             if len(suggestions) == 3:
                 return
-            if node.is_end:
+            if node.is_word:
                 suggestions.append(prefix)
             for c in sorted(node.children):
                 dfs(node.children[c], prefix + c, suggestions)
 
+        
+        output = []
 
         curr = root
         for i, c in enumerate(searchWord):
             if curr:
                 curr = curr.children.get(c)
-                
+
             suggestions = []
             if curr:
                 dfs(curr, searchWord[:i+1], suggestions)
 
             output.append(suggestions)
         return output
-        
