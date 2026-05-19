062c0d02ec773af4b0c8b2a6222818edb6dfffc0
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-19 12:32:24 +0300

    Time: 6 ms (26.51%), Space: 19.1 MB (99.66%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,4 +1,5 @@
 """
+N is length of s, M is length of wordDict, and L is max length of word in wordDict
 leet
 
 leet + "" (empty base case)
@@ -19,24 +20,24 @@ i=0=l
    j=3=t -> is_word
     j+1=4=c = True
 """
+from collections import deque
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        
-        memo = {}
-        def canBreak(i):
-            if i < 0:
+
+        words = set(wordDict)
+
+        seen = set()
+        queue = deque([0])
+        while queue:
+            start = queue.popleft()
+            if start == len(s):
                 return True
 
-            if i in memo:
-                return memo[i]
-
-            for word in wordDict:
-                if s[i - len(word) + 1:i + 1] == word and canBreak(i - len(word)):
-                    memo[i] = True
-                    return True
-
-            memo[i] = False
-            return False
-
-        return canBreak(len(s) - 1)
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in words and end not in seen:
+                    queue.append(end)
+                    seen.add(end)
+        return False
+        
