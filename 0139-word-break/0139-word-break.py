12b83c0097d1e5fdfa8dd7702ce5099affb1e664
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-19 13:14:01 +0300

    Time: 4 ms (40.98%), Space: 19.4 MB (52.1%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -19,25 +19,26 @@ leetcode
 i=0=l
    j=3=t -> is_word
     j+1=4=c = True
+
+01234567
+leetcode
+dp[8] = True
+dp[4] = True
+dp[0] = True
+
 """
 from collections import deque
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-
-        words = set(wordDict)
-
-        seen = set()
-        queue = deque([0])
-        while queue:
-            start = queue.popleft()
-            if start == len(s):
-                return True
-
-            for end in range(start + 1, len(s) + 1):
-                if s[start:end] in words and end not in seen:
-                    queue.append(end)
-                    seen.add(end)
-        return False
+        dp = [False] * (n + 1)
+        dp[n] = True
+        for i in range(n - 1, -1, -1):
+            for word in wordDict:
+                end = i + len(word)
+                if end <= n and s[i:i + len(word)] == word and dp[end]:
+                    dp[i] = True
+                    break
+        return dp[0]
         
