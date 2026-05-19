38c91bff393e60884520819b3ed4a67938347b92
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-05-19 12:20:03 +0300

    Time: 0 ms (100%), Space: 19.4 MB (26.69%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -19,44 +19,24 @@ i=0=l
    j=3=t -> is_word
     j+1=4=c = True
 """
-from collections import deque
-
-class TrieNode:
-    def __init__(self,):
-        self.children = {}
-        self.is_word = False
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         
-        root = TrieNode()
-        for word in wordDict:
-            curr = root
-            for c in word:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-            curr.is_word = True
-
-        seen = set()
-        queue = deque([0])
-        while queue:
-            i = queue.popleft()
-            if i == n:
+        memo = {}
+        def canBreak(i):
+            if i < 0:
                 return True
 
-            curr = root
-            for j in range(i, n):
-                c = s[j]
-                curr = curr.children.get(c)
-                if not curr:
-                    break
+            if i in memo:
+                return memo[i]
 
-                if curr.is_word and (j + 1) not in seen:
-                    seen.add(j + 1)
-                    queue.append(j + 1)
-        return False
+            for word in wordDict:
+                if s[i - len(word) + 1:i + 1] == word and canBreak(i - len(word)):
+                    memo[i] = True
+                    return True
 
+            memo[i] = False
+            return False
 
-        
+        return canBreak(len(s) - 1)
