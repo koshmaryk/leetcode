27f544f593e8b1776e5f0fad319ff1c0f5654b0c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-08 10:47:58 +0300

    Time: 25 ms (55.39%), Space: 30.7 MB (81.42%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,13 +1,23 @@
 class Solution:
+    '''
+    bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+
+    [10,45,-10,-20,0,-25]
+   0 1  2   3   4  5  6
+
+    25
+    [10,55,45,25,25]
+
+    '''
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
         diff = [0] * (n + 1)
-        for first,last,seats in bookings:
+        for first, last, seats in bookings:
             diff[first - 1] += seats
             diff[last] -= seats
 
         answer = [0] * n
-        acc = 0
+        p = 0
         for i in range(n):
-            acc += diff[i]
-            answer[i] = acc
+            p += diff[i]
+            answer[i] = p
         return answer
