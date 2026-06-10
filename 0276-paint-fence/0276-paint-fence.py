bab9b2bb3bfab06d278a543a12a48fbfef495d75
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-10 19:09:46 +0300

    Time: 0 ms (100%), Space: 19.4 MB (32.2%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -1,14 +1,23 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        if n == 1:
-            return k
+        # RR, GG, RG, GR
+        # RRG, GGR, GRG, RGR, GRR, GGR 
+        # (2-1) * (2 + 4) = 1 * 6 = 6
 
-        if n == 2:
-            return k * k
+        memo = {}
 
-        dp = [0] * n
-        dp[0] = k
-        dp[1] = k * k
-        for i in range(2, n):
-            dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2])
-        return dp[n - 1]
+        def dp(i):
+            if i == 1:
+                return k
+
+            if i == 2:
+                return k * k
+
+            if i in memo:
+                return memo[i]
+
+            memo[i] = (k - 1) * (dp(i - 1) + dp(i - 2))
+            return memo[i]
+
+        return dp(n)
+       
