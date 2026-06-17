42bf0bddd055a89b435809253e5aa3f3df4e9937
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-17 21:21:27 +0300

    Time: 1227 ms (15%), Space: 20.1 MB (40.43%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,5 +1,10 @@
 """
+[10,20], [10,35], [30,40], [50,60]
 
+[15,25]
+
+10  15  20  25  30  35  40  50  60 
++2  0   -1  0   +1  -1  -1  +1  -1
 
 s1,e1
 s2,e2
@@ -8,23 +13,27 @@ e2 <= s1 or e1 <= s2
 s1 < e2 and s2 < e1
 
 """
+from sortedcontainers import SortedDict
+
 class MyCalendarTwo:
 
     def __init__(self):
-        self.intervals = []
-        self.overlaps = []
+       self.diff = SortedDict()
+       self.max_overlaps = 2
         
 
     def book(self, startTime: int, endTime: int) -> bool:
-        for s,e in self.overlaps:
-            if startTime < e and s < endTime:
+        self.diff[startTime] = self.diff.get(startTime, 0) + 1
+        self.diff[endTime] = self.diff.get(endTime, 0) - 1
+
+        overlaps = 0
+        for count in self.diff.values():
+            overlaps += count
+            if overlaps > self.max_overlaps:
+                self.diff[startTime] -= 1
+                self.diff[endTime] += 1
                 return False
-
-        for s,e in self.intervals:
-            if startTime < e and s < endTime:
-                self.overlaps.append((max(startTime, s), min(endTime, e)))
-
-        self.intervals.append((startTime, endTime))
+        
         return True
         
 
