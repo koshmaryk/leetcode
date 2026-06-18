11ba17cc4bb08e8df9ef62a50e3a2cd9d0382f64
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-18 13:49:35 +0300

    Time: 566 ms (59.45%), Space: 22.4 MB (13.9%) - LeetHub

--- 0732-my-calendar-iii/0732-my-calendar-iii.py
+++ 0732-my-calendar-iii/0732-my-calendar-iii.py
@@ -1,22 +1,41 @@
-from sortedcontainers import SortedDict
+"""
+
+        0,7
+
+   0,3         4,7
+
+0,1  2,3    4,5  6,7
+
+0 1  2 3    4 5  6 7
+
+
+"""
+from collections import Counter
 
 class MyCalendarThree:
 
     def __init__(self):
-        self.diff = SortedDict()
+        self.vals = Counter()
+        self.lazy = Counter()
         
+    def update(self, startTime, endTime, left = 0, right = 10**9, idx = 1):
+        # case 1: completely outside
+        if startTime > right or endTime < left:
+            return
+
+        # case 1: completely inside
+        if startTime <= left <= right <= endTime:
+            self.vals[idx] += 1
+            self.lazy[idx] += 1 # deffer children update
+        else: # case 3: partially inside
+            mid = (left + right) // 2
+            self.update(startTime, endTime, left, mid, idx * 2)
+            self.update(startTime, endTime, mid + 1, right, idx * 2 + 1)
+            self.vals[idx] = self.lazy[idx] + max(self.vals[idx * 2], self.vals[idx * 2 + 1])
 
     def book(self, startTime: int, endTime: int) -> int:
-        self.diff[startTime] = self.diff.get(startTime, 0) + 1
-        self.diff[endTime] = self.diff.get(endTime, 0) - 1
-        
-        max_overlaps = 0
-        overlaps = 0
-        for count in self.diff.values():
-            overlaps += count
-            max_overlaps = max(max_overlaps, overlaps)
-
-        return max_overlaps
+        self.update(startTime, endTime - 1)
+        return self.vals[1]
         
 
 
