06d0efb06551d9fd1b42451507ff7b7c217c0968
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-18 20:45:14 +0300

    Time: 2264 ms (5.29%), Space: 97.7 MB (11.92%) - LeetHub

---
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+class CountIntervals:
+
+    def __init__(self):
+        self.vals = Counter()
+        self.lazy = Counter()
+
+    def update(self, left, right, start=0, end=10**9, idx=1):
+        if left > end or right < start:
+            return
+
+        if self.lazy[idx]:
+            return
+
+        if left <= start and end <= right:
+            self.vals[idx] = end - start + 1
+            self.lazy[idx] = True
+        else:
+            mid = (start + end) // 2
+            self.update(left, right, start, mid, 2*idx)
+            self.update(left, right, mid + 1, end, 2*idx + 1)
+            self.vals[idx] = self.vals[2*idx] + self.vals[2*idx + 1]
+
+    def add(self, left: int, right: int) -> None:
+        self.update(left, right)
+        
+
+    def count(self) -> int:
+        return self.vals[1]
+        
+
+
+# Your CountIntervals object will be instantiated and called as such:
+# obj = CountIntervals()
+# obj.add(left,right)
+# param_2 = obj.count()
