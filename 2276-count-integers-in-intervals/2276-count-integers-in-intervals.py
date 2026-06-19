de9ba2c81176e0e0dff0e6a56f67d1b44f3788b8
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-19 23:26:59 +0300

    Time: 2547 ms (5.37%), Space: 97.7 MB (12.75%) - LeetHub

--- 2276-count-integers-in-intervals/2276-count-integers-in-intervals.py
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.py
@@ -1,33 +1,37 @@
-from sortedcontainers import SortedList
+from collections import Counter
 
-"""
-
-[1,7]
-
-[5,6]
-
-"""
 class CountIntervals:
 
     def __init__(self):
-        self.intervals = SortedList()
-        self.cnt = 0
+        self.vals = Counter()
+        self.lazy = Counter()
+
+    def update(self, left, right, start=0, end=10**9, idx=1):
+        if self.lazy[idx]:
+            return
+
+        if left > end or right < start:
+            return
+
+        if left <= start and right >= end:
+            self.vals[idx] = end - start + 1
+            self.lazy[idx] = True
+        else:
+            mid = (start + end) // 2
+            self.update(left, right, start, mid, 2*idx)
+            self.update(left, right, mid + 1, end, 2*idx + 1)
+            self.vals[idx] = self.vals[2*idx] + self.vals[2*idx + 1]
 
     def add(self, left: int, right: int) -> None:
-        i = self.intervals.bisect_left((left, -1))
-        if i > 0 and self.intervals[i - 1][1] >= left:
-            i -= 1
-
-        while i < len(self.intervals) and self.intervals[i][0] <= right:
-            l, r = self.intervals[i]
-            left = min(left, l)
-            right = max(right, r)
-            self.cnt -= r - l + 1
-            del self.intervals[i]
-
-        self.intervals.add((left, right))
-        self.cnt += right - left + 1
-       
+        self.update(left, right)
+        
 
     def count(self) -> int:
-        return self.cnt
+        return self.vals[1]
+        
+
+
+# Your CountIntervals object will be instantiated and called as such:
+# obj = CountIntervals()
+# obj.add(left,right)
+# param_2 = obj.count()
