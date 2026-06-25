d70bdb4f02082f2df1f093ae8aaa1110d845c261
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-25 15:29:11 +0300

    Time: 55 ms (68.86%), Space: 24.7 MB (64.72%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,33 +1,38 @@
-from collections import defaultdict
-import heapq 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False
+
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.parent[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        return True
+
 
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for x,y,cost in connections:
-            graph[x].append((y, cost))
-            graph[y].append((x, cost))
+        connections.sort(key=lambda x:x[2])
 
-        in_mst = [False] * (n + 1)
-        pq = [(0, 1)] # weight, ->node
+        uf = UnionFind(n + 1)
 
         min_cost = cnt = 0
-        while pq:
-            cost, city = heapq.heappop(pq)
+        for x,y,cost in connections:
+            if uf.union(x, y):
+                min_cost += cost
+                cnt += 1
 
-            if in_mst[city]:
-                continue
-
-            min_cost += cost
-            in_mst[city] = True
-            cnt += 1
-
-            if cnt == n:
-                return min_cost
-
-            for next_city, next_cost in graph[city]:
-                if not in_mst[next_city]:
-                    heapq.heappush(pq, (next_cost, next_city))
-        
+                if cnt == n - 1:
+                    return min_cost
         return -1
-        
