2380780f3cabe8699fe16447826570a5792e8e4b
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-25 13:23:05 +0300

    Time: 661 ms (69.02%), Space: 82.9 MB (72.09%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,47 +1,26 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [0] * n
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rx, ry = self.find(x), self.find(y)
-        if rx == ry:
-            return False
-
-        if self.rank[rx] < self.rank[ry]:
-            rx, ry = ry, rx
-        self.parent[ry] = rx
-        if self.rank[rx] < self.rank[ry]:
-            self.rank[rx] += 1
-        return True
+import heapq 
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
 
-        edges = []
-        for u in range(n):
-            for v in range(u + 1, n):
-                weight = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
-                edges.append((weight, u, v))
+        pq = [(0, 0)] # weight, node (index)
+        visited = [False] * n
         
-        edges.sort()
-
-        uf = UnionFind(n)
         cost = cnt = 0
-        for weight, u, v in edges:
-            if uf.union(u, v):
-                cost += weight
-                cnt += 1
+        while cnt < n:
+            weight, node = heapq.heappop(pq)
 
-                if cnt == n - 1:
-                    break
-            
+            if visited[node]:
+                continue
+
+            visited[node] = True
+            cost += weight
+            cnt += 1
+
+            for next_node in range(n):
+                if not visited[next_node]:
+                    next_weight = abs(points[node][0] - points[next_node][0]) + abs(points[node][1] - points[next_node][1])
+
+                    heapq.heappush(pq, (next_weight, next_node))
         return cost
-
-        
