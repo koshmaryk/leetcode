c4e49d288c4a4916977442b18edc757e6cc48fa3
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-06-29 17:26:16 +0300

    Time: 93 ms (99.2%), Space: 46.3 MB (76.09%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,40 @@
+"""
+
+0,0
+1,3
+2,5
+3,7
+
+
+"""
+import bisect
+from math import inf
+
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.id = 0
+        self.elements = [[(0,0)] for _ in range(length)]
+        
+
+    def set(self, index: int, val: int) -> None:
+        self.elements[index].append((self.id, val))
+        
+
+    def snap(self) -> int:
+        self.id += 1
+        return self.id - 1
+        
+
+    def get(self, index: int, snap_id: int) -> int:
+        i = bisect.bisect_right(self.elements[index], (snap_id, inf))
+        _, val = self.elements[index][i - 1]
+        return val
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
