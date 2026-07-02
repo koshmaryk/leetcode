2e4901c7e82acd4fd542f59c32489882f2bb5828
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-07-02 13:31:44 +0300

    Time: 24 ms (88.98%), Space: 19.5 MB (78.81%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.py
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.py
@@ -1,29 +1,42 @@
+import heapq
 from math import inf
 
 
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
         n, m = len(workers), len(bikes)
-        memo = {}
 
-        def dist(i, j):
+        def distance(i, j):
             return abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1])
 
+        
+        def count_ones(mask):
+            cnt = 0
+            while mask:
+                mask &= (mask - 1)
+                cnt += 1
+            return cnt
 
-        def dp(i, mask):
+
+        dist = [inf] * (1 << m)
+        dist[0] = 0
+
+        pq = [(0, 0)] # cost, mask
+        while pq:
+            cost, mask = heapq.heappop(pq)
+            if cost > dist[mask]:
+                continue
+
+            i = count_ones(mask)
             if i == n:
-                return 0
+                return cost
 
-            if mask in memo:
-                return memo[mask]
-
-            best_cost = inf
             for j in range(m):
                 if not (mask & (1 << j)):
-                    best_cost = min(best_cost, dist(i, j) + dp(i + 1, mask | (1 << j)))
-            memo[mask] = best_cost
-            return best_cost
-
-
-        return dp(0, 0)
+                    new_mask = mask | (1 << j)
+                    new_cost = cost + distance(i, j)
+                    if new_cost < dist[new_mask]:
+                        dist[new_mask] = new_cost
+                        heapq.heappush(pq, (new_cost, new_mask))
+        return -1
          
