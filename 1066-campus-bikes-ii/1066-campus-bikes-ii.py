2ad64a6bcf84ff2a2984a2c1dcf2289f54b22666
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-07-02 12:59:18 +0300

    Time: 31 ms (76.27%), Space: 20.7 MB (55.08%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.py
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.py
@@ -4,30 +4,26 @@ from math import inf
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
         n, m = len(workers), len(bikes)
-
-        def count_ones(mask):
-            cnt = 0
-            while mask:
-                mask &= (mask - 1)
-                cnt += 1
-            return cnt
+        memo = {}
 
         def dist(i, j):
             return abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1])
 
 
-        dp = [inf] * (1 << m)
-        dp[0] = 0
+        def dp(i, mask):
+            if i == n:
+                return 0
 
-        best = inf
-        for mask in range(1 << m):
-            i = count_ones(mask)
-            if i >= n:
-                best = min(best, dp[mask])
-                continue
+            if mask in memo:
+                return memo[mask]
 
+            best_cost = inf
             for j in range(m):
                 if not (mask & (1 << j)):
-                    new_mask = mask | (1 << j)
-                    dp[new_mask] = min(dp[new_mask], dp[mask] + dist(i, j))
-        return best
+                    best_cost = min(best_cost, dist(i, j) + dp(i + 1, mask | (1 << j)))
+            memo[mask] = best_cost
+            return best_cost
+
+
+        return dp(0, 0)
+         
