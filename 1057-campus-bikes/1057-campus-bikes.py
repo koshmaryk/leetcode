d8fe4f30246bf8bfcd61f218b0e7ac2cd57f6440
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-07-02 15:10:21 +0300

    Time: 612 ms (70.13%), Space: 115.1 MB (95.48%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -5,23 +5,21 @@ class Solution:
         def distance(x1, y1, x2, y2):
             return abs(x1 - x2) + abs(y1 - y2)
 
-        pairs = []
+        buckets = [[] for _ in range(2001)]
         for worker, (x1, y1) in enumerate(workers):
             for bike, (x2, y2) in enumerate(bikes):
-                pairs.append((distance(x1, y1, x2, y2), worker, bike))
+                dist = distance(x1, y1, x2, y2)
+                buckets[dist].append((worker, bike))
 
-        pairs.sort()
-        
         bike_status = [False] * m
         answer = [-1] * n
         cnt = 0
-        for _, worker, bike in pairs:
-            if answer[worker] == -1 and not bike_status[bike]:
-                bike_status[bike] = True
-                answer[worker] = bike
-                cnt += 1
-
-            if cnt == n:
-                break
-
+        for bucket in buckets:
+            for (worker, bike) in bucket:
+                if answer[worker] == -1 and not bike_status[bike]:
+                    bike_status[bike] = True
+                    answer[worker] = bike
+                    cnt += 1
+                    if cnt == n:
+                        break
         return answer
