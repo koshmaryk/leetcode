9236104ff7340cb038644acefb54a77b1ee7b04e
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-07-06 15:56:31 +0300

    Time: 83 ms (61.29%), Space: 30.3 MB (76.83%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -12,19 +12,17 @@ class Solution:
         max_prob = [0.0] * n
         max_prob[start_node] = 1.0
 
-        for _ in range(n - 1):
-            updated = False
-            for i, (u,v) in enumerate(edges):
-                if succProb[i] * max_prob[u] > max_prob[v]:
-                    max_prob[v] = succProb[i] * max_prob[u]
-                    updated = True
+        pq = [(-1.0, start_node)]
+        while pq:
+            prob, node = heapq.heappop(pq)
+            if node == end_node:
+                return -prob
 
-                if succProb[i] * max_prob[v] > max_prob[u]:
-                    max_prob[u] = succProb[i] * max_prob[v]
-                    updated = True
+            for next_prob, next_node in graph[node]:
+                new_prob = next_prob * -prob
+                if new_prob > max_prob[next_node]:
+                    max_prob[next_node] = new_prob
+                    heapq.heappush(pq, (-new_prob, next_node))
+        return 0.0
 
-            if not updated:
-                break
-
-        return max_prob[end_node]
         
