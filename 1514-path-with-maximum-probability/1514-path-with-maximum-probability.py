f2cd90d5808a5b3d49ecefc81c96ecb3bca29237
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-07-08 12:50:53 +0300

    Time: 90 ms (48.71%), Space: 30.1 MB (85.58%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,28 +1,36 @@
-from collections import defaultdict
-import heapq
+from collections import defaultdict, deque
 
 
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         graph = defaultdict(list)
         for i, (u,v) in enumerate(edges):
-            graph[u].append((succProb[i], v))
-            graph[v].append((succProb[i], u))
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
 
         max_prob = [0.0] * n
         max_prob[start_node] = 1.0
 
-        pq = [(-1.0, start_node)]
-        while pq:
-            prob, node = heapq.heappop(pq)
-            if node == end_node:
-                return -prob
+        in_queue = [False] * n
+        in_queue[start_node] = True
 
-            for next_prob, next_node in graph[node]:
-                new_prob = next_prob * -prob
-                if new_prob > max_prob[next_node]:
-                    max_prob[next_node] = new_prob
-                    heapq.heappush(pq, (-new_prob, next_node))
-        return 0.0
+        queue = deque([start_node])
+        while queue:
+            node = queue.popleft()
+            in_queue[node] = False
+
+            for next_node, next_prob in graph[node]:
+                if max_prob[node] * next_prob > max_prob[next_node]:
+                    max_prob[next_node] = max_prob[node] * next_prob
+                    if not in_queue[next_node]:
+                        queue.append(next_node)
+                        in_queue[next_node] = True
+
+        return max_prob[end_node]
+                    
+
+      
+
+                
 
         
