282bef6a0d6c45c866d11ee72e18ba2a9b2283aa
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2026-07-20 22:15:34 +0300

    Time: 708 ms (52.69%), Space: 20.7 MB (68.99%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.py
@@ -1,6 +1,6 @@
 """
-TC O(n) # n * 2 * 26 * log 26
-SC O(1) # 26^2
+TC O((26 + m) log 26 + n) # O((26 + m) log 26 * n)
+SC O(26^2 + (26 + m))
 
 """
 from collections import defaultdict
@@ -13,27 +13,20 @@ class Solution:
         n = len(source)
         m = len(original)
 
-        graph = [[] for _ in range(26)]
+        dist = [[inf] * 26 for _ in range(26)]
+        for s in range(26):
+            dist[s][s] = 0
+
         for i in range(m):
             u = ord(original[i]) - ord('a')
             v = ord(changed[i]) - ord('a')
-            graph[u].append((v, cost[i]))
+            dist[u][v] = min(dist[u][v], cost[i])
 
-        def dijkstra(s):
-            pq = [(0, s)]
-            dist = [inf] * 26
-            dist[s] = 0
-            while pq:
-                d, c = heapq.heappop(pq)
-
-                for next_c, next_d in graph[c]:
-                    new_d = next_d + d
-                    if new_d < dist[next_c]:
-                        dist[next_c] = new_d
-                        heapq.heappush(pq, (new_d, next_c))
-            return dist
-
-        dist = [dijkstra(s) for s in range(26)]
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    if dist[i][k] + dist[k][j] < dist[i][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
 
         total_cost = 0
         for i in range(n):
@@ -42,5 +35,4 @@ class Solution:
             if dist[s][t] == inf:
                 return -1
             total_cost += dist[s][t]
-
         return total_cost
